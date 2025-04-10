28364785253c361e81d13c55cf1669f42e8fabbb
Cleiton viana <estudo.cleiton@gmail.com>
2025-04-10 20:08:54 -0300

    feat(name): Value object que representa o nome dos usuários no sistema

---
+++ src/modules/customer/value-object/name.spec.ts
@@ -0,0 +1,103 @@
+import { Name } from './name';
+import { TechnicalError } from "../../../shared/error/technical.error";
+
+describe('Name', () => {
+    describe('create', () => {
+        describe('should create a valid', () => {
+            const successCases = [
+                { name: 'john', scenario: 'with minimum length' },
+                { name: 'mark', scenario: 'with another valid name' },
+                { name: 'abcdefghijklmnopqrstuv', scenario: 'with maximum length' }
+            ];
+
+            successCases.forEach(({ name, scenario }) => {
+                it(`Name object ${scenario}`, () => {
+                    // Act
+                    const result = Name.create(name);
+
+                    // Assert
+                    expect(result.value.value).toBe(name);
+                });
+            });
+        });
+
+        describe('should fail to create an invalid', () => {
+            const failureCases = [
+                { name: null as unknown as string, scenario: 'when name is null', errorCodeExpected: 'PROPERTY_CANNOT_BE_NULL' },
+                { name: undefined as unknown as string, scenario: 'when name is undefined', errorCodeExpected: 'PROPERTY_CANNOT_BE_NULL' },
+                { name: '', scenario: 'when name is empty', errorCodeExpected: 'FIELD_CANNOT_BE_EMPTY' },
+                { name: 'ab', scenario: 'when name is too short', errorCodeExpected: 'FIELD_WITH_INVALID_SIZE' },
+                { name: 'abcdefghijklmnopqrstuvwxyz', scenario: 'when name is too long', errorCodeExpected: 'FIELD_WITH_INVALID_SIZE' },
+                { name: 'john123', scenario: 'when name contains invalid characters', errorCodeExpected: 'NAME_WITH_INVALID_FORMAT' },
+                { name: 'John #$$', scenario: 'when name contains special characters', errorCodeExpected: 'NAME_WITH_INVALID_FORMAT' }
+            ];
+
+            failureCases.forEach(({ name, scenario, errorCodeExpected }) => {
+                it(`Name object ${scenario}`, () => {
+                    // Act
+                    const result = Name.create(name);
+                    const failures = result.failures
+
+                    // Assert
+                    expect(failures.length).toBe(1);
+                    expect(failures[0].code).toBe(errorCodeExpected);
+                });
+            });
+        });
+    });
+
+    describe('hydrate', () => {
+        it('should create a Name object without validation', () => {
+            // Arrange
+            const nameString = 'anyname';
+
+            // Act
+            const result = Name.hydrate(nameString);
+
+            // Assert
+            expect(result).toBeInstanceOf(Name);
+            expect(result.value).toBe(nameString);
+        });
+
+        it('should throw an error when name is null or undefined', () => {
+            // Arrange
+            const values = [null, undefined];
+
+            // Act
+            values.forEach((value) => {
+                expect(() => Name.hydrate(value)).toThrow(TechnicalError);
+            })
+        });
+    });
+
+    describe('equal', () => {
+        it('should return true when names are equal', () => {
+            // Arrange
+            const nameString = 'john';
+            const result1 = Name.create(nameString);
+            const result2 = Name.create(nameString);
+
+            // Assert
+            expect(result1.value.equal(result2.value)).toBe(true);
+        });
+
+        it('should return false when names are different', () => {
+            // Arrange
+            const result1 = Name.create('john');
+            const result2 = Name.create('mark');
+
+            // Assert
+            expect(result1.value.equal(result2.value)).toBe(false);
+
+        });
+
+        it('should return false when comparing with non-Name object', () => {
+            // Arrange
+            const result = Name.create('john');
+            const notNameObject = { name: 'john' };
+
+            // Assert
+            expect(result.value.equal(notNameObject as any)).toBe(false);
+        });
+    });
+});
---
+++ src/modules/customer/value-object/name.ts
@@ -0,0 +1,41 @@
+import { failure, Result, success } from "../../../shared/result/result";
+import { Assert, Flow } from "../../../shared/assert/assert";
+import { not } from "../../../shared/assert/not";
+import { is } from "../../../shared/assert/is";
+import { SimpleFailure } from "../../../shared/failure/simple.failure.type";
+import { TechnicalError } from "../../../shared/error/technical.error";
+
+export class Name {
+
+  private constructor(
+    public readonly value: string
+  ) {}
+
+  public static create(name: string): Result<Name> {
+    const failures: SimpleFailure[] = []
+    const minNameLength = 3;
+    const maxNameLength = 24;
+
+    Assert.all(
+      failures,
+      { field: "name" },
+      not.null(name, "PROPERTY_CANNOT_BE_NULL", {}, Flow.stop),
+      not.empty(name, "FIELD_CANNOT_BE_EMPTY", {}, Flow.stop),
+      is.between(name, minNameLength, maxNameLength, "FIELD_WITH_INVALID_SIZE", {}, Flow.stop),
+      is.match(name, /^[a-zA-Z]{3,24}$/, "NAME_WITH_INVALID_FORMAT"),
+    );
+
+    return (failures.length > 0)
+      ? failure(failures)
+      : success(new Name(name));
+  }
+
+  public static hydrate(name: string): Name {
+    TechnicalError.if(!name, 'NULL_ARGUMENT')
+    return new Name(name)
+  }
+
+  public equal(other: Name): boolean {
+    return (other instanceof Name && other.value === this.value)
+  }
+}
