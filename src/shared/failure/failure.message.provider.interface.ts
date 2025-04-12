884a73b252067549894981203c5101860f9a0794
Cleiton viana <estudo.cleiton@gmail.com>
2025-04-12 16:39:13 -0300

    refactor(IFailureMessageProvider): Move para um arquivo próprio a definição da interface de uma failure provider.

---
+++ src/shared/failure/failure.message.provider.interface.ts
@@ -0,0 +1,9 @@
+import { FailureMessageConfig } from "./failure.message.provider";
+
+/**
+ * Interface para provedores de mensagens de falha
+ */
+export interface IFailureMessageProvider {
+    getMessageConfig(code: string): FailureMessageConfig | undefined;
+  }
+  
