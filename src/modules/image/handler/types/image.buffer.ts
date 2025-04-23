15af67fd989b918a3a3dfae5a21a4c3c9ede530c
Cleiton viana <estudo.cleiton@gmail.com>
2025-04-23 15:27:40 -0300

    feat(image): Adiciona tipos `ImageBuffer`, `ImageResizedDimension` e `ImageResizedBuffer` para manipulação de imagens

---
+++ src/modules/image/handler/types/image.buffer.ts
@@ -0,0 +1,21 @@
+import { ImageSize } from "./image.size";
+
+export type ImageBuffer = {
+  small: Buffer;
+  normal: Buffer;
+  large: Buffer;
+};
+
+export type ImageResizedDimension = {
+  ratio: number; // O aspect ratio numérico usado para os cálculos (targetWidth / targetHeight)
+  small: ImageSize;
+  normal: ImageSize;
+  large: ImageSize;
+};
+
+// Buffers redimensionados
+export type ImageResizedBuffer = {
+  small: Buffer;
+  normal: Buffer;
+  large: Buffer;
+};
