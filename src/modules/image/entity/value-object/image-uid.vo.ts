5f489872904749a0084297f4220653e6363608c4
Cleiton viana <estudo.cleiton@gmail.com>
2025-04-23 15:16:10 -0300

    feat(ImageUID): Cria novo método para criar um imageUID a partir de um uuidv4.

--- src/modules/image/entity/value-object/image-uid.vo.ts
+++ src/modules/image/entity/value-object/image-uid.vo.ts
@@ -1,5 +1,15 @@
 import { UID } from "../../../../shared/value-object/uid";
+import { failure, Result, success } from "../../../../shared/result/result";
+import { isUIDv4 } from "../../../../shared/validator/validator";
 
 export class ImageUID extends UID {
   protected static readonly PREFIX: string = 'IMG'
+
+  public static generate(uid: string): Result<ImageUID> {
+    return isUIDv4(uid)
+      ? success(new ImageUID(uid))
+      : failure({
+        code: 'invalidUidProvided',
+      })
+  }
 }
