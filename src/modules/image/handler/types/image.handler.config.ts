86288ee7aab4d430190fe01d2239e5a242b14ea2
Cleiton viana <estudo.cleiton@gmail.com>
2025-04-23 15:28:29 -0300

    feat(ImageHandlerConfig): Adiciona tipo `ImageHandlerConfig` e enums para configuração de processamento de imagem

---
+++ src/modules/image/handler/types/image.handler.config.ts
@@ -0,0 +1,30 @@
+import { ImageSize } from "./image.size";
+
+// Configuração para o processamento de imagem
+export type ImageHandlerConfig = {
+  compress: CompressionType;
+  extension: ImageExtension;
+  aspect: AspectRatio; // Aspect ratio *desejado* para a saída
+  sizes: { // Larguras desejadas para cada tamanho
+    small: ImageSize;
+    normal: ImageSize;
+    large: ImageSize;
+  };
+};
+
+export enum CompressionType {
+  Lossless = "Lossless",
+  Lossy = "Lossy",
+}
+
+export enum ImageExtension {
+  JPEG = "image/jpeg",
+  PNG = "image/png",
+  WEBP = "image/webp",
+}
+
+export enum AspectRatio {
+  Square = '1:1',
+  Landscape = '16:9',
+  Portrait = '9:16',
+}
