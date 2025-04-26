24e5e585949d0a5abdd02b861a152e7e01f72b0e
Cleiton viana <estudo.cleiton@gmail.com>
2025-04-25 21:01:50 -0300

    refactor(MovieUID): Atualiza os imports das dependências.

--- src/modules/movie/entity/value-object/movie.uid.ts
+++ src/modules/movie/entity/value-object/movie.uid.ts
@@ -1,4 +1,4 @@
-import { UID } from "src/shared/value-object/uid";
+import { UID } from "../../../../shared/value-object/uid";
 
 export class MovieUID extends UID {
     protected static readonly PREFIX: string = 'MOVIE'
