d58ff5b6d498dc279647dc3fdfe3c4857de872a2
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-01 12:42:52 -0300

    docs(PersonUID): adiciona documentação a este value object.

--- src/modules/person/value-object/person.uid.ts
+++ src/modules/person/value-object/person.uid.ts
@@ -1,5 +1,15 @@
 import { UID } from "../../../shared/value-object/uid";
 
+/**
+ * Representa o identificador único de uma pessoa no sistema.
+ *
+ * Esta classe estende a classe base UID e define um prefixo específico
+ * para identificadores de pessoas, permitindo que sejam facilmente
+ * reconhecidos e validados no sistema.
+ *
+ * O prefixo "PRSN" é usado para diferenciar IDs de pessoas de outros
+ * tipos de identificadores no sistema.
+ */
 export class PersonUID extends UID {
-    protected static readonly PREFIX: string = 'PRSN'
+  protected static readonly PREFIX: string = "PRSN";
 }
