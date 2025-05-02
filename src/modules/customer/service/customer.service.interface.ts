f2c3ebdf83338d87ee8e59b2e799d1703b673260
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-02 11:56:49 -0300

    style(ICustomerService): Ajusta a formatação do código conforme o padrão do linter.

--- src/modules/customer/service/customer.service.interface.ts
+++ src/modules/customer/service/customer.service.interface.ts
@@ -1,20 +1,20 @@
 import { Result } from "src/shared/result/result";
-import {Customer} from "../entity/customer";
+import { Customer } from "../entity/customer";
 
 export type CreateCustomerProps = {
-  name: string,
-  birthDate: Date,
-  email: string 
-}
+  name: string;
+  birthDate: Date;
+  email: string;
+};
 
 export type UpdateCustomerProps = {
-  name?: string,
-  birthDate?: Date,
-  email?: string,
-}
+  name?: string;
+  birthDate?: Date;
+  email?: string;
+};
 
 export interface ICustomerService {
-  findByEmail(email: string): Promise<Result<Customer>>
+  findByEmail(email: string): Promise<Result<Customer>>;
   findById(uid: string): Promise<Result<Customer>>;
   create(user: CreateCustomerProps): Promise<Result<Customer>>;
   update(uid: string, props: UpdateCustomerProps): Promise<Result<Customer>>;
