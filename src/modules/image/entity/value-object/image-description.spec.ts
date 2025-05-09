45951e21a7832337355c34cd66b2f18788c937a3
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-09 10:37:56 -0300

    refactor(ImageDescription): Refatora para utilizar os códigos de erro de `FailureCodes`.

--- src/modules/image/entity/value-object/image-description.spec.ts
+++ src/modules/image/entity/value-object/image-description.spec.ts
@@ -1,42 +1,40 @@
 import { ImageDescription } from "./image-description";
 import { faker } from "@faker-js/faker/.";
-import { codes } from "../../../../shared/value-object/multilingual-content";
+import { FailureCode } from "../../../../shared/failure/failure.codes.enum";
 
 describe("ImageDescription", () => {
   const validPtDescription = "Descrição da Imagem com tamanho válido";
   const validEnDescription = "Valid image description with proper length";
-  const tooShortDescription = "Short"; 
+  const tooShortDescription = "Short";
   const tooLongDescription = faker.string.alphanumeric(241);
   const descriptionWithValidCharacters = ".,!?-_-+/<>#@$%&*";
   const descriptionWithInvalidCharacters = "Descrição com caracter inválido ¥";
 
   describe("ImageDescription", () => {
-
-    describe('create', () => {
-
+    describe("create", () => {
       describe("deve retornar uma instância de ImageDescription com sucesso", () => {
         const successCases = [
           {
             contents: [
               { language: "pt", text: faker.string.alphanumeric(8) },
-              { language: "en", text: validEnDescription }
+              { language: "en", text: validEnDescription },
             ],
-            scenario: "com o tamanho mínimo exato"
+            scenario: "com o tamanho mínimo exato",
           },
           {
             contents: [
               { language: "pt", text: validPtDescription },
-              { language: "en", text: faker.string.alphanumeric(240) }
+              { language: "en", text: faker.string.alphanumeric(240) },
             ],
-            scenario: "com o tamanho máximo exato"
+            scenario: "com o tamanho máximo exato",
           },
           {
             contents: [
               { language: "pt", text: descriptionWithValidCharacters },
-              { language: "en", text: descriptionWithValidCharacters }
+              { language: "en", text: descriptionWithValidCharacters },
             ],
-            scenario: "com caracteres especiais permitidos"
-          }
+            scenario: "com caracteres especiais permitidos",
+          },
         ];
 
         successCases.forEach(({ contents, scenario }) => {
@@ -56,26 +54,26 @@ describe("ImageDescription", () => {
           {
             contents: [
               { language: "pt", text: tooShortDescription },
-              { language: "en", text: validEnDescription }
+              { language: "en", text: validEnDescription },
             ],
             scenario: "com menos que o tamanho mínimo",
-            errorCode: codes.contentLengthOutOfRange
+            errorCode: FailureCode.STRING_LENGTH_OUT_OF_RANGE,
           },
           {
             contents: [
               { language: "pt", text: validPtDescription },
-              { language: "en", text: tooLongDescription }
+              { language: "en", text: tooLongDescription },
             ],
             scenario: "com mais que o tamanho máximo",
-            errorCode: codes.contentLengthOutOfRange
-          }, 
+            errorCode: FailureCode.STRING_LENGTH_OUT_OF_RANGE,
+          },
           {
             contents: [
               { language: "pt", text: descriptionWithInvalidCharacters },
-              { language: "en", text: descriptionWithValidCharacters }
+              { language: "en", text: descriptionWithValidCharacters },
             ],
             scenario: "com caracteres inválidos na descrição",
-            errorCode: codes.contentInvalidFormat
+            errorCode: FailureCode.STRING_INVALID_FORMAT,
           },
         ];
 
--- src/modules/image/entity/value-object/image-description.ts
+++ src/modules/image/entity/value-object/image-description.ts
@@ -4,5 +4,6 @@ export class ImageDescription extends MultilingualContent {
   protected static readonly MIN_LENGTH: number = 8;
   protected static readonly MAX_LENGTH: number = 240;
 
-  protected static readonly FORMAT_REGEX = /^[A-Za-zÀ-ÖØ-öø-ÿ\d\s\-_.,<>?!@#$%&*+\/]+$/;
+  protected static readonly FORMAT_REGEX =
+    /^[A-Za-zÀ-ÖØ-öø-ÿ\d\s\-_.,<>?!@#$%&*+\/]+$/;
 }
