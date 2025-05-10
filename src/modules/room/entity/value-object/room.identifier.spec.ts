c16bb9e58c04e139f01519d776ab185d1971bbe4
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-10 19:02:02 -0300

    feat(RoomIdentifier): Cria um value object para representar o número da sala.

---
+++ src/modules/room/entity/value-object/room.identifier.spec.ts
@@ -0,0 +1,130 @@
+import { RoomIdentifier } from "./room.identifier";
+import { FailureCode } from "../../../../shared/failure/failure.codes.enum";
+import { TechnicalError } from "../../../../shared/error/technical.error";
+
+describe("RoomIdentifier", () => {
+  describe("create", () => {
+    describe("casos de sucesso", () => {
+      const successCase = [
+        {
+          scenario: "ID igual oa valor mínimo permitido",
+          input: 1,
+        },
+        {
+          scenario: "ID igual ao vlaor máximo permito",
+          input: 100,
+        },
+      ];
+
+      successCase.forEach(({ scenario, input }) => {
+        it(scenario, () => {
+          // Act
+          const result = RoomIdentifier.create(input);
+
+          // Assert
+          expect(result.invalid).toBe(false);
+          expect(result.value.value).toBe(input);
+        });
+      });
+    });
+
+    describe("casos de falha", () => {
+      const successCase = [
+        {
+          scenario: "ID for inválido",
+          input: -1,
+          code: FailureCode.VALUE_OUT_OF_RANGE,
+        },
+        {
+          scenario: "ID é exatamente o valor máximo permitido + 1",
+          input: 101,
+          code: FailureCode.VALUE_OUT_OF_RANGE,
+        },
+        {
+          scenario: "ID é um número decimal",
+          input: 1.5,
+          code: FailureCode.VALUE_NOT_INTEGER,
+        },
+      ];
+
+      successCase.forEach(({ scenario, input, code }) => {
+        it(scenario, () => {
+          // Act
+          const result = RoomIdentifier.create(input);
+
+          // Assert
+          expect(result.invalid).toBe(true);
+          expect(result.failures.length).toBe(1);
+          expect(result.failures[0].code).toBe(code);
+        });
+      });
+    });
+  });
+
+  describe("hydrate", () => {
+    it("deve instanciar com sucesso", () => {
+      // Arrange
+      const values = [1, 2, 99, 100];
+
+      // Act
+      values.forEach((value) => {
+        const identifier = RoomIdentifier.hydrate(value);
+
+        // Assert
+        expect(identifier.value).toBe(value);
+      });
+    });
+
+    it("deve lançar um erro ao receber parãmetro inválido", () => {
+      expect(() => RoomIdentifier.hydrate(null)).toThrow(TechnicalError);
+    });
+  });
+
+  describe("equal", () => {
+    it("deve retornar true quando os identificadores são iguais", () => {
+      // Arrange
+      const id1 = RoomIdentifier.hydrate(10);
+      const id2 = RoomIdentifier.hydrate(10);
+
+      // Act
+      const result = id1.equals(id2);
+
+      // Assert
+      expect(result).toBe(true);
+    });
+
+    it("deve retornar false quando os identificadores são diferentes", () => {
+      // Arrange
+      const id1 = RoomIdentifier.hydrate(10);
+      const id2 = RoomIdentifier.hydrate(20);
+
+      // Act
+      const result = id1.equals(id2);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    it("deve retornar false quando o outro identificador é nulo", () => {
+      // Arrange
+      const id1 = RoomIdentifier.hydrate(10);
+
+      // Act
+      const result = id1.equals(null);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    it("deve retornar false quando o outro identificador é indefinido", () => {
+      // Arrange
+      const id1 = RoomIdentifier.hydrate(10);
+
+      // Act
+      const result = id1.equals(undefined);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+  });
+});
---
+++ src/modules/room/entity/value-object/room.identifier.ts
@@ -0,0 +1,44 @@
+import { Result, success, failure } from "../../../../shared/result/result";
+import { SimpleFailure } from "../../../../shared/failure/simple.failure.type";
+import { Validate } from "../../../../shared/validator/validate";
+import { TechnicalError } from "../../../../shared/error/technical.error";
+import { isNull } from "../../../../shared/validator/validator";
+import { FailureCode } from "../../../../shared/failure/failure.codes.enum";
+
+export class RoomIdentifier {
+  private static readonly MIN_VALUE = 1;
+  private static readonly MAX_VALUE = 100;
+
+  private constructor(public readonly value: number) {}
+
+  public static create(roomIdentifier: number): Result<RoomIdentifier> {
+    const failures: SimpleFailure[] = [];
+
+    Validate.number(roomIdentifier)
+      .field("roomIdentifier")
+      .failures(failures)
+      .isRequired()
+      .isInteger()
+      .isInRange(this.MIN_VALUE, this.MAX_VALUE);
+
+    return failures.length > 0
+      ? failure(failures)
+      : success(new RoomIdentifier(roomIdentifier));
+  }
+
+  public static hydrate(roomIdentifier: number): RoomIdentifier {
+    TechnicalError.if(
+      isNull(roomIdentifier),
+      FailureCode.INVALID_HYDRATE_DATA,
+      {
+        vo: "RoomIdentifier",
+      },
+    );
+    return new RoomIdentifier(roomIdentifier);
+  }
+
+  public equals(other?: RoomIdentifier): boolean {
+    if (other === null || other === undefined) return false;
+    return this.value === other.value;
+  }
+}
