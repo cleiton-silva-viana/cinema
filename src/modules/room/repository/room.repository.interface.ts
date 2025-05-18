224fc95ea364b954a4404b8191176209d6fabf5b
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-18 18:46:10 -0300

    feat(IRoomRepository): Adiciona novos métodos `roomExists`, `addBooking` e `deleteBooking`.

--- src/modules/room/repository/room.repository.interface.ts
+++ src/modules/room/repository/room.repository.interface.ts
@@ -1,8 +1,12 @@
 import { Room } from "../entity/room";
+import { BookingSlot } from "../entity/value-object/booking.slot";
 
 export interface IRoomRepository {
+  roomExists(roomID: number): Promise<boolean>;
   findById(roomID: number): Promise<Room>;
   create(room: Room): Promise<Room>;
   update(roomID: number, room: Partial<Room>): Promise<Room>;
   delete(roomID: number): Promise<null>;
+  addBooking(roomID: number, booking: BookingSlot): Promise<Room>;
+  deleteBooking(roomID: number, bookingUID: string): Promise<null>;
 }
