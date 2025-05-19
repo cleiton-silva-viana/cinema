88d40bda873f2018d71b7437532b69a503e1b5dd
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-19 14:18:36 -0300

    feat(/movie.constant): Adicionado constantes para mecanismo de injeção de dependências do módulo `Movie`.

--- src/modules/movie/constant/movie.constant.ts
+++ src/modules/movie/constant/movie.constant.ts
@@ -3,3 +3,5 @@
  * Utilizada para injeção de dependência no NestJS.
  */
 export const MOVIE_REPOSITORY = "MOVIE_REPOSITORY";
+export const DOMAIN_MOVIE_SERVICE = "DOMAIN_MOVIE_SERVICE";
+export const APPLICATION_MOVIE_SERVICE = "APPLICATION_MOVIE_SERVICE";
