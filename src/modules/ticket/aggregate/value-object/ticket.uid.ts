4fe3eafad99d1798a743c4b97f20660aba6887fc
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-19 22:14:07 -0300

    feat(TicketUID): Criar value obejct que representa a identificação de um ticket de cinema.

---
+++ src/modules/ticket/aggregate/value-object/ticket.uid.ts
@@ -0,0 +1,23 @@
+import { UID } from "../../../../shared/value-object/uid";
+
+/**
+ * Representa o identificador único de um ingresso no sistema.
+ * 
+ * Esta classe estende a classe base UID e define um prefixo específico
+ * para identificadores de ingressos, garantindo que todos os IDs de ingressos
+ * sejam facilmente identificáveis e distinguíveis de outros tipos de IDs.
+ * 
+ * @example
+ * // Criar um novo UID para um ingresso
+ * const ticketId = TicketUID.create();
+ * 
+ * // Hidratar um UID existente
+ * const existingTicketId = TicketUID.hydrate("tckt_abc123");
+ */
+export class TicketUID extends UID {
+  /**
+   * Prefixo usado para todos os identificadores de ingressos.
+   * Todos os IDs de ingressos começarão com "tckt_".
+   */
+  protected static readonly PREFIX: string = "tckt";
+}
