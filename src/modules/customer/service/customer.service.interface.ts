7ddd1e4538f8e44e32b1396264c67d4db4a3b177
Cleiton viana <estudo.cleiton@gmail.com>
2025-05-21 20:47:39 -0300

    refactor(ICustomerService): Alterada a assinatura do método `delete` para retornar um booleano.

--- src/modules/customer/service/customer.service.interface.ts
+++ src/modules/customer/service/customer.service.interface.ts
@@ -1,22 +1,22 @@
 import { Result } from "src/shared/result/result";
 import { Customer } from "../entity/customer";
 
-export type CreateCustomerProps = {
+export interface ICreateCustomerProps {
   name: string;
   birthDate: Date;
   email: string;
 };
 
-export type UpdateCustomerProps = {
-  name?: string;
+export interface IUpdateCustomerProps {
+  name?: string
   birthDate?: Date;
   email?: string;
 };
 
-export interface ICustomerService {
+export interface ICustomerDomainService {
   findByEmail(email: string): Promise<Result<Customer>>;
   findById(uid: string): Promise<Result<Customer>>;
-  create(user: CreateCustomerProps): Promise<Result<Customer>>;
-  update(uid: string, props: UpdateCustomerProps): Promise<Result<Customer>>;
-  delete(uid: string): Promise<Result<null>>;
+  create(user: ICreateCustomerProps): Promise<Result<Customer>>;
+  update(uid: string, props: IUpdateCustomerProps): Promise<Result<Customer>>;
+  delete(uid: string): Promise<Result<boolean>>;
 }
