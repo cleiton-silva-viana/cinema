30509a1f6a1e7fe3e27fb45b89562151d07bd17e
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 19:21:37 -0300

    test(ImageSizes): Adiciona mais casos de testes para o método `hyrate`.

--- src/modules/image/entity/value-object/image.sizes.spec.ts
+++ src/modules/image/entity/value-object/image.sizes.spec.ts
@@ -1,48 +1,49 @@
-import { ImageSizes } from "./image.sizes";
-import { faker } from "@faker-js/faker";
-import { FailureCode } from "../../../../shared/failure/failure.codes.enum";
-import { SimpleFailure } from "../../../../shared/failure/simple.failure.type";
-import { validateAndCollect } from "../../../../shared/validator/common.validators";
+import { faker } from '@faker-js/faker'
+import { ICreateImageSizesInput, ImageSizes } from './image.sizes'
+import { FailureCode } from '@shared/failure/failure.codes.enum'
+import { SimpleFailure } from '@shared/failure/simple.failure.type'
+import { validateAndCollect } from '@shared/validator/common.validators'
+import { TechnicalError } from '@shared/error/technical.error'
 
-describe("ImageSizes", () => {
+describe('ImageSizes', () => {
   const sizes = {
     small: faker.internet.url(),
     normal: faker.internet.url(),
     large: faker.internet.url(),
-  };
+  }
 
-  describe("create", () => {
-    let failures: SimpleFailure[];
+  describe('create', () => {
+    let failures: SimpleFailure[]
 
     beforeEach(() => {
-      failures = [];
-    });
+      failures = []
+    })
 
-    it("deve retornar uma instância de ImageSizes com sucesso", () => {
+    it('deve retornar uma instância de ImageSizes com sucesso', () => {
       // Act
-      const result = validateAndCollect(ImageSizes.create(sizes), failures);
+      const result = validateAndCollect(ImageSizes.create(sizes), failures)
 
       // Assert
-      expect(result).toBeDefined();
-      expect(result.small).toBe(sizes.small);
-      expect(result.normal).toBe(sizes.normal);
-      expect(result.large).toBe(sizes.large);
-    });
+      expect(result).toBeDefined()
+      expect(result.small).toBe(sizes.small)
+      expect(result.normal).toBe(sizes.normal)
+      expect(result.large).toBe(sizes.large)
+    })
 
-    describe("deve retornar um erro quando os tamanhos são inválidos", () => {
+    describe('deve retornar um erro quando os tamanhos são inválidos', () => {
       const failureCases = [
         {
           sizes: null,
-          scenario: "quando o objeto sizes é nulo",
+          scenario: 'quando o objeto sizes é nulo',
           errorCode: FailureCode.MISSING_REQUIRED_DATA,
         },
         {
           sizes: {
-            small: "        ",
+            small: '        ',
             normal: sizes.normal,
             large: sizes.large,
           },
-          scenario: "quando há uma URL vazia",
+          scenario: 'quando há uma URL vazia',
           errorCode: FailureCode.STRING_CANNOT_BE_EMPTY,
         },
 
@@ -52,34 +53,75 @@ describe("ImageSizes", () => {
             normal: sizes.normal,
             large: null,
           },
-          scenario: "quando há uma URL nula",
+          scenario: 'quando há uma URL nula',
           errorCode: FailureCode.MISSING_REQUIRED_DATA,
         },
-      ];
+      ]
 
       failureCases.forEach(({ sizes, scenario, errorCode }) => {
         it(`deve rejeitar tamanhos de imagem ${scenario}`, () => {
           // Act
-          const result = validateAndCollect(ImageSizes.create(sizes), failures);
+          const result = validateAndCollect(ImageSizes.create(sizes as ICreateImageSizesInput), failures)
 
           // Assert
-          expect(result).toBeNull();
-          expect(failures[0].code).toBe(errorCode);
-        });
-      });
-    });
-  });
+          expect(result).toBeNull()
+          expect(failures[0].code).toBe(errorCode)
+        })
+      })
+    })
+  })
 
-  describe("hydrate", () => {
-    it("deve criar uma instância de ImageSizes diretamente", () => {
+  describe('hydrate', () => {
+    it('deve criar uma instância de ImageSizes diretamente', () => {
       // Act
-      const result = ImageSizes.hydrate(sizes);
+      const result = ImageSizes.hydrate(sizes)
 
       // Assert
-      expect(result).toBeInstanceOf(ImageSizes);
-      expect(result.small).toBe(sizes.small);
-      expect(result.normal).toBe(sizes.normal);
-      expect(result.large).toBe(sizes.large);
-    });
-  });
-});
+      expect(result).toBeInstanceOf(ImageSizes)
+      expect(result.small).toBe(sizes.small)
+      expect(result.normal).toBe(sizes.normal)
+      expect(result.large).toBe(sizes.large)
+    })
+
+    it('deve lançar um erro quando o input for nullo', () => {
+      expect(() => ImageSizes.hydrate(null as any)).toThrow(TechnicalError)
+    })
+
+    describe('deve retornar um erro quando os tamanhos são inválidos', () => {
+      const failureCases = [
+        {
+          scenario: 'quando o tamanho "smal" é inválido',
+          sizes: {
+            small: null,
+          },
+        },
+        {
+          scenario: 'quando o tamanho "normal" é inválido',
+          sizes: {
+            normal: null,
+          },
+        },
+        {
+          scenario: 'quando o tamanho "large" é inválido',
+          sizes: {
+            large: null,
+          },
+        },
+      ]
+      failureCases.forEach(({ sizes, scenario }) => {
+        it(`deve rejeitar tamanhos de imagem ${scenario}`, () => {
+          // Arrange
+          const s = {
+            small: faker.internet.url(),
+            normal: faker.internet.url(),
+            large: faker.internet.url(),
+            ...sizes,
+          } as unknown as ICreateImageSizesInput
+
+          // Act & Assert
+          expect(() => ImageSizes.hydrate(s)).toThrow(TechnicalError)
+        })
+      })
+    })
+  })
+})
