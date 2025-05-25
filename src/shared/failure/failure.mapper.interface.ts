d0a6419728c02b255ef5758e7a3966d46ff36ec2
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 13:32:49 -0300

    style(IFailureMapper): aplica linter.

--- src/shared/failure/failure.mapper.interface.ts
+++ src/shared/failure/failure.mapper.interface.ts
@@ -1,10 +1,8 @@
-import { RichFailure } from "./rich.failure.type";
-import { SimpleFailure } from "./simple.failure.type";
+import { RichFailure } from './rich.failure.type'
+import { SimpleFailure } from './simple.failure.type'
 
 export interface IFailureMapper {
-  toRichFailure(failure: SimpleFailure, language?: "pt" | "en"): RichFailure;
-  toRichFailures(
-    failures: SimpleFailure[],
-    language?: "pt" | "en",
-  ): RichFailure[];
+  toRichFailure(failure: SimpleFailure, language?: 'pt' | 'en'): RichFailure
+
+  toRichFailures(failures: SimpleFailure[], language?: 'pt' | 'en'): RichFailure[]
 }
