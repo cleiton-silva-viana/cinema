dd3310fd5d14b8aa2a04190bdb46baf46a3ae865
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 18:14:34 -0300

    refactor(ICustomerRepository): Aplicando linter...

--- src/modules/customer/repository/customer.repository.interface.ts
+++ src/modules/customer/repository/customer.repository.interface.ts
@@ -1,11 +1,15 @@
-import { Customer } from "../entity/customer";
-import { CustomerUID } from "../entity/value-object/customer.uid";
-import { Email } from "../entity/value-object/email";
+import { Customer } from '../entity/customer'
+import { CustomerUID } from '../entity/value-object/customer.uid'
+import { Email } from '../entity/value-object/email'
 
 export interface ICustomerRepository {
-  findById(uid: CustomerUID): Promise<Customer>;
-  findByEmail(email: Email): Promise<Customer>;
-  create(customer: Customer): Promise<Customer>;
-  update(uid: CustomerUID, customer: Customer): Promise<Customer>;
-  delete(uid: CustomerUID): Promise<null>;
+  findById(uid: CustomerUID): Promise<Customer>
+
+  findByEmail(email: Email): Promise<Customer>
+
+  create(customer: Customer): Promise<Customer>
+
+  update(uid: CustomerUID, customer: Customer): Promise<Customer>
+
+  delete(uid: CustomerUID): Promise<null>
 }
