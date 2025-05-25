942013e2b45b50aa68edc6e2fa72cc72ea0c1a52
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 19:20:53 -0300

    style(ImageSizes): Aplica linter ao arquivo.

--- src/modules/image/entity/value-object/image.sizes.ts
+++ src/modules/image/entity/value-object/image.sizes.ts
@@ -1,19 +1,19 @@
-import { failure, Result, success } from "../../../../shared/result/result";
-import { SimpleFailure } from "../../../../shared/failure/simple.failure.type";
-import { Validate } from "../../../../shared/validator/validate";
-import { TechnicalError } from "../../../../shared/error/technical.error";
+import { failure, Result, success } from '@shared/result/result'
+import { SimpleFailure } from '@shared/failure/simple.failure.type'
+import { Validate } from '@shared/validator/validate'
+import { TechnicalError } from '@shared/error/technical.error'
 
 /**
  * Interface que define os tamanhos de imagem necessários
  * Cada propriedade representa uma URL para uma versão da imagem
  */
-export interface Sizes {
+export interface ICreateImageSizesInput {
   /** URL da imagem em tamanho pequeno */
-  small: string;
+  small: string
   /** URL da imagem em tamanho normal/médio */
-  normal: string;
+  normal: string
   /** URL da imagem em tamanho grande */
-  large: string;
+  large: string
 }
 
 /**
@@ -24,7 +24,7 @@ export class ImageSizes {
   private constructor(
     public readonly small: string,
     public readonly normal: string,
-    public readonly large: string,
+    public readonly large: string
   ) {}
 
   /**
@@ -32,24 +32,16 @@ export class ImageSizes {
    * @param sizes Objeto contendo as URLs para os diferentes tamanhos de imagem
    * @returns Result contendo a instância de ImageSizes ou falhas de validação
    */
-  public static create(sizes: Sizes): Result<ImageSizes> {
-    const failures: SimpleFailure[] = [];
+  public static create(sizes: ICreateImageSizesInput): Result<ImageSizes> {
+    const failures: SimpleFailure[] = []
 
     Validate.object({ sizes }, failures)
       .isRequired()
-      .property("small", () =>
-        this.validateSize({ size_small: sizes.small }, failures),
-      )
-      .property("normal", () =>
-        this.validateSize({ size_normal: sizes.normal }, failures),
-      )
-      .property("large", () =>
-        this.validateSize({ size_large: sizes.large }, failures),
-      );
+      .property('small', () => this.validateSize({ size_small: sizes.small }, failures))
+      .property('normal', () => this.validateSize({ size_normal: sizes.normal }, failures))
+      .property('large', () => this.validateSize({ size_large: sizes.large }, failures))
 
-    return failures.length > 0
-      ? failure(failures)
-      : success(new ImageSizes(sizes.small, sizes.normal, sizes.large));
+    return failures.length > 0 ? failure(failures) : success(new ImageSizes(sizes.small, sizes.normal, sizes.large))
   }
 
   /**
@@ -60,18 +52,14 @@ export class ImageSizes {
    * @returns Instância de ImageSizes
    * @throws Error se algum dos tamanhos for nulo ou indefinido
    */
-  public static hydrate(sizes: {
-    small: string;
-    normal: string;
-    large: string;
-  }): ImageSizes {
-    TechnicalError.validateRequiredFields({ sizes });
+  public static hydrate(sizes: { small: string; normal: string; large: string }): ImageSizes {
+    TechnicalError.validateRequiredFields({ sizes })
     TechnicalError.validateRequiredFields({
       sizes_small: sizes.small,
       size_normal: sizes.normal,
       sizes_large: sizes.large,
-    });
-    return new ImageSizes(sizes.small, sizes.normal, sizes.large);
+    })
+    return new ImageSizes(sizes.small, sizes.normal, sizes.large)
   }
 
   /**
@@ -79,10 +67,7 @@ export class ImageSizes {
    * @param size Valor do tamanho (URL)
    * @param failures Array para armazenar as falhas
    */
-  private static validateSize(
-    size: Record<string, string>,
-    failures: SimpleFailure[],
-  ): void {
-    Validate.string(size, failures).isRequired().isNotEmpty();
+  private static validateSize(size: Record<string, string>, failures: SimpleFailure[]): void {
+    Validate.string(size, failures).isRequired().isNotEmpty()
   }
 }
