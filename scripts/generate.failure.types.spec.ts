97cf8b8a9cbb0778d7f4e4207ec9263008417ac3
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 12:50:10 -0300

    refactor(script/generate.failure): Refatora para incluir os imports no código gerado dinâmicamente..

---
+++ scripts/generate.failure.types.spec.ts
@@ -0,0 +1,199 @@
+import {faker} from '@faker-js/faker/.'
+import {
+    analyzeFailureTemplate,
+    extractTemplateVariables,
+    generateCodeConstant,
+    generateFailureCodes,
+} from './generate.failure.types'
+import {FailureTemplate} from 'src/shared/failure/failure.template'
+
+function generateTemplate(template?: { pt: string; en: string }) {
+    return {
+        title: {
+            pt: faker.lorem.lines(1),
+            en: faker.lorem.lines(1),
+        },
+        template: template || {
+            pt: faker.lorem.lines(1),
+            en: faker.lorem.lines(1),
+        },
+        status: 400,
+    }
+}
+
+describe('extractTemplateVariables', () => {
+    it('deve retornar um array contendo todas as variáveis contída sno template', () => {
+        // Arrange
+        const fieldName = 'field'
+        const fieldType = 'string'
+        const field = `${fieldName}:${fieldType}`
+        const template = `Este campo '{${field}}' deve ser recuperado pelo array.`
+
+        // Act
+        const result = extractTemplateVariables(template)
+
+        // Assert
+        expect(result.length).toBe(1)
+        expect(result[0].field).toBe(fieldName)
+        expect(result[0].type).toBe(fieldType)
+    })
+
+    it('deve retornar um valor do tipo any se o valor do field for desconhecido', () => {
+        // Arrange
+        const fieldName = 'property'
+        const fieldType = 'property'
+        const field = `${fieldName}:${fieldType}`
+        const template = `Este é um template com uma {${field}} de tipo desconhecido`
+
+        // Act
+        const result = extractTemplateVariables(template)
+
+        // Assert
+        expect(result.length).toBe(1)
+        expect(result[0].field).toBe(fieldName)
+        expect(result[0].type).toBe('any')
+    })
+
+    it('deve retornar um tipo any quando não for fornecido uma tipagem para a variável', () => {
+        // Arrange
+        const fieldName = 'property'
+        const template = `A variável deste template deve ser do tipo any {${fieldName}}`
+
+        // Act
+        const result = extractTemplateVariables(template)
+
+        // Assert
+        expect(result.length).toBe(1)
+        expect(result[0].field).toBe(fieldName)
+        expect(result[0].type).toBe('any')
+    })
+
+    it('deve retornar um array vazio se não houver qualquer variável no template', () => {
+        // Arrange
+        const template = 'sem valores para serem recuperados'
+
+        // Act
+        const result = extractTemplateVariables(template)
+
+        // Assert
+        expect(result.length).toBe(0)
+    })
+
+    it('deve retornar mais de uma propriedade corretamente', () => {
+        // Arrange
+        const field1 = {name: 'name', type: 'string'}
+        const field3 = {name: 'max', type: 'number'}
+        const field4 = {name: 'hour', type: 'any'}
+        const template = `{${field1.name}:${field1.type}} - {${field3.name}:${field3.type}} - {${field4.name}:${field4.type}}`
+
+        // Act
+        const result = extractTemplateVariables(template)
+
+        // Assert
+        expect(result.length).toBe(3)
+        expect(result.some((f) => f.field === field1.name && f.type === field1.type))
+        expect(result.some((f) => f.field === field3.name && f.type === field3.type))
+        expect(result.some((f) => f.field === field4.name && f.type === field4.type))
+    })
+})
+
+describe('analyzeFailureTemplate', () => {
+    it('deve retornar um array contendo todos as variáveis contidas nos templates em pt e en', () => {
+        // Arrange
+        const min = 'min'
+        const max = 'max'
+        const resource = 'resource'
+        const type = 'type'
+        const count = 'count'
+
+        const temp = generateTemplate({
+            en: `This {${type}} must have length between {${min}} of {${max}}.`,
+            pt: `Este {${resource}} deve ter {${count}} valores.`,
+        })
+
+        // Act
+        const result = analyzeFailureTemplate(temp)
+
+        // Assert
+        expect(result.length).toBe(5)
+    })
+
+    it('deve retornar os campos duplicados no template apenas 1 vez', () => {
+        // Arrange
+        const min = 'min'
+        const max = 'max'
+        const type = 'type'
+
+        const temp = generateTemplate({
+            en: `This {${type}} must have length between {${min}} of {${max}}.`,
+            pt: `O {${type}} deve ter uma tamanho entre {${min}} e {${max}}.`,
+        })
+
+        // Act
+        const result = analyzeFailureTemplate(temp)
+
+        // Assert
+        expect(result.length).toBe(3)
+    })
+
+    it('deve retornar um array vazio se não houver variáveis no template', () => {
+        // Arrange
+        const template = generateTemplate()
+
+        // Act
+        const result = analyzeFailureTemplate(template)
+
+        // Assert
+        expect(result.length).toBe(0)
+        expect(result).toEqual([])
+    })
+})
+
+describe('generateFailureCodes', () => {
+    it('deve gerar os códigos de erro ordenamos corretamente', () => {
+        // Arrange
+        const failures: Record<string, FailureTemplate> = {
+            A_FIRST: generateTemplate(),
+            Z_LAST: generateTemplate(),
+            B_SECOND: generateTemplate(),
+            F_THIRD: generateTemplate(),
+        }
+
+        // Act
+        const result = generateFailureCodes(failures)
+
+        // Assert
+        expect(result).toBeDefined()
+        expect(result).toContain('A_FIRST')
+        expect(result).toContain('F_THIRD')
+        expect(result).toContain('Z_LAST')
+        expect(result).toContain('B_SECOND')
+        expect(result).toContain('FailureCode')
+    })
+})
+
+describe('generateCodeConstant', () => {
+    it('deve criar ', () => {
+        // Arrange
+        const failures: Record<string, FailureTemplate> = {
+            WITHOUT_VAR: generateTemplate({
+                pt: 'Sem propriedade alguma',
+                en: 'Without any property',
+            }),
+            WITH_ONE_VAR: generateTemplate({
+                pt: 'Com uma {name}, property',
+                en: 'With one {name} property',
+            }),
+            WITH_TWO_VAR: generateTemplate({
+                pt: 'Com uma {name}, {property}',
+                en: 'With one {name} {property}',
+            }),
+        }
+
+        // Act
+        const result = generateCodeConstant(failures)
+
+        // Assert
+        expect(result).toBeDefined()
+    })
+})
---
+++ scripts/generate.failure.types.ts
@@ -0,0 +1,100 @@
+import {FailureTemplate} from 'src/shared/failure/failure.template'
+
+type Failures = Record<string, FailureTemplate>
+type TemplateVariable = { field: string; type: string }
+
+/**
+ * Extrai todas as variáveis de um template usando regex
+ * @Example "O campo '{field:string}' deve ter entre {min:number} e {max:number}" -> [ {field: 'field', type: 'string'} ]
+ * */
+export function extractTemplateVariables(template: string): Array<TemplateVariable> {
+    const regexp = /\{(\w+)(?::(\w+))?\}/g
+    const variables: Array<{ field: string; type: string }> = []
+    let match
+
+    while ((match = regexp.exec(template)) !== null) {
+        const field = match[1]
+        let type = match[2] || 'any'
+
+        if (!['string', 'number', 'Date'].includes(type)) type = 'any'
+
+        if (!variables.some((v) => v.field === field)) {
+            variables.push({field, type})
+        }
+    }
+
+    return variables
+}
+
+/**
+ * Analisa um erro e retorna todas as variáveis necessárias
+ * */
+export function analyzeFailureTemplate(err: FailureTemplate): Array<TemplateVariable> {
+    const ptVar = extractTemplateVariables(err.template.pt)
+    const enVar = extractTemplateVariables(err.template.en)
+
+    const uniqueVars = new Map<string, TemplateVariable>()
+
+    ptVar.forEach((variable) => {
+        uniqueVars.set(variable.field, variable)
+    })
+
+    enVar.forEach((variable) => {
+        uniqueVars.set(variable.field, variable)
+    })
+
+    return Array.from(uniqueVars.values())
+}
+
+/**
+ * Gera uma string de enum contendo todos os códigos de erro contídos no objeto passado por parâmetro
+ * */
+export function generateFailureCodes(failures: Failures): string {
+    const codes = Object.keys(failures)
+        .sort()
+        .map((code) => `  ${code} = '${code}'`)
+        .join(',\n')
+
+    return `export enum FailureCode {\n${codes}\n}\n// Este arquivo é gerado automaticamente. NÃO EDITE MANUALMENTE!`
+}
+
+/**
+ * Gera uma constante contendo as funções de criação de simpleFailure
+ * */
+export function generateCodeConstant(failures: Failures): string {
+    const entries = Object.entries(failures)
+
+    let codes: string = ''
+
+    entries.forEach(([code, template]) => {
+        let params = analyzeFailureTemplate(template)
+        if (params.length === 0) {
+            codes += `${code}: (): SimpleFailure => ({ 
+        code: FailureCode.${code}, 
+        details: {} 
+      }),\n`
+            return
+        }
+
+        let functionArguments: string = ''
+        let detailsKey: string = ''
+        params.forEach((p) => {
+            const variable = `${p.field}: ${p.type}`
+            functionArguments += functionArguments.length === 0 ? variable : `, ${variable}`
+            detailsKey += detailsKey === '' ? p.field : `, ${p.field}`
+        })
+
+        codes += `${code}: (${functionArguments}): SimpleFailure => ({ 
+      code: FailureCode.${code}, 
+      details: { ${detailsKey} }
+    }),\n`
+    })
+
+    return `
+  import { FailureCode } from './failure.codes.enum'
+  import { SimpleFailure } from './simple.failure.type'
+  
+  export const FailureFactory = {
+    ${codes}
+  }`
+}
