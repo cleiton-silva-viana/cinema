dd3991bb6d95abce98a74fbfc901ec7bc2002bcc
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 19:03:52 -0300

    feat(ICustomerRepository): Adiciona métodos `hasEmail`, `hasCPF` e `hasStudentCardID`.

--- src/modules/customer/repository/customer.repository.interface.ts
+++ src/modules/customer/repository/customer.repository.interface.ts
@@ -1,15 +1,22 @@
 import { Customer } from '../entity/customer'
 import { CustomerUID } from '../entity/value-object/customer.uid'
 import { Email } from '../entity/value-object/email'
+import { CPF } from '@/modules/customer/entity/value-object/cpf'
 
 export interface ICustomerRepository {
+  hasEmail(email: Email): Promise<boolean>
+
+  hasCPF(cpf: CPF): Promise<boolean>
+
+  hasStudentCardID(id: string): Promise<boolean>
+
   findById(uid: CustomerUID): Promise<Customer>
 
   findByEmail(email: Email): Promise<Customer>
 
   create(customer: Customer): Promise<Customer>
 
-  update(uid: CustomerUID, customer: Customer): Promise<Customer>
+  update(uid: CustomerUID, customer: Partial<Customer>): Promise<Partial<Customer>>
 
   delete(uid: CustomerUID): Promise<null>
 }
