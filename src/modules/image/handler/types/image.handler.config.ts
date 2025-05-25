62af87590602f3aa610446efc2ef00c13476256b
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 19:29:01 -0300

    style(ImageHandlerConfig): Aplica linter.

--- src/modules/image/handler/types/image.handler.config.ts
+++ src/modules/image/handler/types/image.handler.config.ts
@@ -1,31 +1,31 @@
-import { ImageSize } from "./image.size";
+import { ImageSize } from './image.size'
 
 // Configuração para o processamento de imagem
 export type ImageHandlerConfig = {
-  compress: CompressionType;
-  extension: ImageExtension;
-  aspect: AspectRatio; // Aspect ratio *desejado* para a saída
+  compress: CompressionType
+  extension: ImageExtension
+  aspect: AspectRatio // Aspect ratio *desejado* para a saída
   sizes: {
     // Larguras desejadas para cada tamanho
-    small: ImageSize;
-    normal: ImageSize;
-    large: ImageSize;
-  };
-};
+    small: ImageSize
+    normal: ImageSize
+    large: ImageSize
+  }
+}
 
 export enum CompressionType {
-  Lossless = "Lossless",
-  Lossy = "Lossy",
+  Lossless = 'Lossless',
+  Lossy = 'Lossy',
 }
 
 export enum ImageExtension {
-  JPEG = "image/jpeg",
-  PNG = "image/png",
-  WEBP = "image/webp",
+  JPEG = 'image/jpeg',
+  PNG = 'image/png',
+  WEBP = 'image/webp',
 }
 
 export enum AspectRatio {
-  Square = "square",
-  Landscape = "landscape", // 16:9
-  Portrait = "portrait", // 9:16
+  Square = 'square',
+  Landscape = 'landscape', // 16:9
+  Portrait = 'portrait', // 9:16
 }
