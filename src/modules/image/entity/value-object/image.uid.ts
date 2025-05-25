5e962dd5107c447cee5eb947f32b1593167bc860
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 19:09:34 -0300

    style(ImageUID): Aplica linter ao arquivo.

--- src/modules/image/entity/value-object/image.uid.ts
+++ src/modules/image/entity/value-object/image.uid.ts
@@ -1,5 +1,5 @@
-import { UID } from "../../../../shared/value-object/uid";
+import { UID } from '@shared/value-object/uid'
 
 export class ImageUID extends UID {
-  protected static readonly PREFIX: string = "IMG";
+  protected static readonly PREFIX: string = 'IMG'
 }
