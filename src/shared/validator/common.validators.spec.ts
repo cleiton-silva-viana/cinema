1790377bf44d36480d0c40fc6aa2b6b5ea53760f
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-26 12:07:32 -0300

    style(common.validators): Adiciona nova função `hydrateEnum`.

--- src/shared/validator/common.validators.spec.ts
+++ src/shared/validator/common.validators.spec.ts
@@ -1,364 +1,362 @@
 import {
-  ensureNotNull,
   collectNullFields,
-  validateAndCollect,
-  parseToEnum,
+  ensureNotNull,
+  hydrateEnum,
   Mode,
-} from "./common.validators";
-import { FailureCode } from "../failure/failure.codes.enum";
-import { faker } from "@faker-js/faker";
-import { failure, Result, success } from "../result/result";
-import { SimpleFailure } from "../failure/simple.failure.type";
+  parseToEnum,
+  validateAndCollect,
+} from './common.validators'
+import { faker } from '@faker-js/faker'
+import { FailureCode } from '../failure/failure.codes.enum'
+import { failure, Result, success } from '../result/result'
+import { SimpleFailure } from '../failure/simple.failure.type'
 
-describe("Common Validators", () => {
-  describe("ensureNotNull", () => {
-    it("deve retornar um array vazio quando não há campos nulos", () => {
+describe('Common Validators', () => {
+  describe('ensureNotNull', () => {
+    it('deve retornar um array vazio quando não há campos nulos', () => {
       // Arrange
-      const validField = faker.string.alphanumeric();
+      const validField = faker.string.alphanumeric()
 
       // Act
       const failures = ensureNotNull({
         campo: validField,
-      });
+      })
 
       // Assert
-      expect(failures).toBeInstanceOf(Array);
-      expect(failures.length).toBe(0);
-    });
+      expect(failures).toBeInstanceOf(Array)
+      expect(failures.length).toBe(0)
+    })
 
-    it("deve adicionar falha quando o valor é null", () => {
+    it('deve adicionar falha quando o valor é null', () => {
       // Arrange
-      const fieldName = "nullField";
+      const fieldName = 'nullField'
 
       // Act
       const failures = ensureNotNull({
         [fieldName]: null,
-      });
+      })
 
       // Assert
-      expect(failures.length).toBe(1);
-      expect(failures[0].code).toBe(FailureCode.MISSING_REQUIRED_DATA);
-      expect(failures[0].details.field).toBe(fieldName);
-    });
+      expect(failures.length).toBe(1)
+      expect(failures[0].code).toBe(FailureCode.MISSING_REQUIRED_DATA)
+      expect(failures[0].details?.field).toBe(fieldName)
+    })
 
-    it("deve adicionar falha quando o valor é undefined", () => {
+    it('deve adicionar falha quando o valor é undefined', () => {
       // Arrange
-      let undefinedValue;
-      const fieldName = "undefinedField";
+      let undefinedValue
+      const fieldName = 'undefinedField'
 
       // Act
       const failures = ensureNotNull({
         [fieldName]: undefinedValue,
-      });
+      })
 
       // Assert
-      expect(failures.length).toBe(1);
-      expect(failures[0].code).toBe(FailureCode.MISSING_REQUIRED_DATA);
-      expect(failures[0].details.field).toBe(fieldName);
-    });
+      expect(failures.length).toBe(1)
+      expect(failures[0].code).toBe(FailureCode.MISSING_REQUIRED_DATA)
+      expect(failures[0].details?.field).toBe(fieldName)
+    })
 
-    it("deve validar múltiplos campos corretamente", () => {
+    it('deve validar múltiplos campos corretamente', () => {
       // Arrange
-      const validField = faker.string.alphanumeric();
+      const validField = faker.string.alphanumeric()
 
       // Act
       const failures = ensureNotNull({
         validField: validField,
         nullField: null,
         undefinedField: undefined,
-      });
+      })
 
       // Assert
-      expect(failures.length).toBe(2);
+      expect(failures.length).toBe(2)
 
-      const fieldNames = failures.map((failure) => failure.details.field);
-      expect(fieldNames).toContain("nullField");
-      expect(fieldNames).toContain("undefinedField");
-      expect(fieldNames).not.toContain("validField");
+      const fieldNames = failures.map((failure) => failure.details?.field)
+      expect(fieldNames).toContain('nullField')
+      expect(fieldNames).toContain('undefinedField')
+      expect(fieldNames).not.toContain('validField')
 
       failures.forEach((failure) => {
-        expect(failure.code).toBe(FailureCode.MISSING_REQUIRED_DATA);
-      });
-    });
-  });
+        expect(failure.code).toBe(FailureCode.MISSING_REQUIRED_DATA)
+      })
+    })
+  })
 
-  describe("collectNullFields", () => {
-    it("deve retornar um array vazio quando não há campos nulos", () => {
+  describe('collectNullFields', () => {
+    it('deve retornar um array vazio quando não há campos nulos', () => {
       // Arrange
       const obj = {
         name: faker.person.firstName(),
         age: faker.date.birthdate(),
         email: faker.internet.email(),
-      };
+      }
 
       // Act
-      const resultado = collectNullFields(obj);
+      const resultado = collectNullFields(obj)
 
       // Assert
-      expect(resultado).toEqual([]);
-      expect(resultado.length).toBe(0);
-    });
+      expect(resultado).toEqual([])
+      expect(resultado.length).toBe(0)
+    })
 
-    it("deve retornar os nomes dos campos nulos", () => {
+    it('deve retornar os nomes dos campos nulos', () => {
       // Arrange
       const obj = {
         name: faker.person.firstName(),
         age: null as any,
         email: undefined as any,
         address: faker.location.streetAddress(),
-      };
+      }
 
       // Act
-      const resultado = collectNullFields(obj);
+      const resultado = collectNullFields(obj)
 
       // Assert
-      expect(resultado).toContain("age");
-      expect(resultado).toContain("email");
-      expect(resultado.length).toBe(2);
-    });
+      expect(resultado).toContain('age')
+      expect(resultado).toContain('email')
+      expect(resultado.length).toBe(2)
+    })
 
-    it("deve lidar com objetos vazios", () => {
+    it('deve lidar com objetos vazios', () => {
       // Arrange
-      const campos = {};
+      const campos = {}
 
       // Act
-      const resultado = collectNullFields(campos);
+      const resultado = collectNullFields(campos)
 
       // Assert
-      expect(resultado).toEqual([]);
-      expect(resultado.length).toBe(0);
-    });
+      expect(resultado).toEqual([])
+      expect(resultado.length).toBe(0)
+    })
 
-    it("deve lidar com valores falsy que não são null ou undefined", () => {
+    it('deve lidar com valores falsy que não são null ou undefined', () => {
       // Arrange
       const campos = {
         zero: 0,
         falsy: false,
-        emptyString: "",
+        emptyString: '',
         nil: null as any,
         indefinido: undefined as any,
-      };
+      }
 
       // Act
-      const resultado = collectNullFields(campos);
+      const resultado = collectNullFields(campos)
 
       // Assert
-      expect(resultado).toContain("nil");
-      expect(resultado).toContain("indefinido");
-      expect(resultado).not.toContain("zero");
-      expect(resultado).not.toContain("falsy");
-      expect(resultado).not.toContain("emptyString");
-      expect(resultado.length).toBe(2);
-    });
-  });
+      expect(resultado).toContain('nil')
+      expect(resultado).toContain('indefinido')
+      expect(resultado).not.toContain('zero')
+      expect(resultado).not.toContain('falsy')
+      expect(resultado).not.toContain('emptyString')
+      expect(resultado.length).toBe(2)
+    })
+  })
 
-  describe("validateAndCollect", () => {
+  describe('validateAndCollect', () => {
     const failure1 = {
       code: FailureCode.MISSING_REQUIRED_DATA,
       details: {
-        field: "Erro 1",
+        field: 'Erro 1',
       },
-    };
+    }
     const failure2 = {
       code: FailureCode.MISSING_REQUIRED_DATA,
       details: {
-        field: "Erro 2",
+        field: 'Erro 2',
       },
-    };
+    }
 
-    it("deve retornar o valor e não adicionar falhas quando o resultado é um sucesso", () => {
+    it('deve retornar o valor e não adicionar falhas quando o resultado é um sucesso', () => {
       // Arrange
-      const successValue = { id: 1, name: "Test Item" };
-      const result: Result<typeof successValue> = success(successValue);
-      const failures: SimpleFailure[] = [];
+      const successValue = { id: 1, name: 'Test Item' }
+      const result: Result<typeof successValue> = success(successValue)
+      const failures: SimpleFailure[] = []
 
       // Act
-      const collectedValue = validateAndCollect(result, failures);
+      const collectedValue = validateAndCollect(result, failures)
 
       // Assert
-      expect(collectedValue).toBe(successValue);
-      expect(failures.length).toBe(0);
-    });
+      expect(collectedValue).toBe(successValue)
+      expect(failures.length).toBe(0)
+    })
 
-    it("deve retornar null e adicionar a falha ao array quando o resultado é uma falha com um único erro", () => {
+    it('deve retornar null e adicionar a falha ao array quando o resultado é uma falha com um único erro', () => {
       // Arrange
-      const result: Result<never> = failure(failure1);
-      const failures: SimpleFailure[] = [];
+      const result: Result<never> = failure(failure1)
+      const failures: SimpleFailure[] = []
 
       // Act
-      const collectedValue = validateAndCollect(result, failures);
+      const collectedValue = validateAndCollect(result, failures)
 
       // Assert
-      expect(collectedValue).toBeNull();
-      expect(failures.length).toBe(1);
-      expect(failures).toContainEqual(failure1);
-    });
+      expect(collectedValue).toBeNull()
+      expect(failures.length).toBe(1)
+      expect(failures).toContainEqual(failure1)
+    })
 
-    it("deve retornar null e adicionar as falhas ao array quando o resultado é uma falha com múltiplos erros", () => {
+    it('deve retornar null e adicionar as falhas ao array quando o resultado é uma falha com múltiplos erros', () => {
       // Arrange
-      const errors: SimpleFailure[] = [failure2, failure1];
-      const result: Result<never> = failure(errors);
-      const failures: SimpleFailure[] = [];
+      const errors: SimpleFailure[] = [failure2, failure1]
+      const result: Result<never> = failure(errors)
+      const failures: SimpleFailure[] = []
 
       // Act
-      const collectedValue = validateAndCollect(result, failures);
+      const collectedValue = validateAndCollect(result, failures)
 
       // Assert
-      expect(collectedValue).toBeNull();
-      expect(failures.length).toBe(2);
-      expect(failures).toEqual(expect.arrayContaining(errors));
-    });
+      expect(collectedValue).toBeNull()
+      expect(failures.length).toBe(2)
+      expect(failures).toEqual(expect.arrayContaining(errors))
+    })
 
-    it("deve anexar falhas a um array de falhas pré-existente quando o resultado é uma falha", () => {
+    it('deve anexar falhas a um array de falhas pré-existente quando o resultado é uma falha', () => {
       // Arrange
-      const preExistingError = failure1;
-      const newError = failure2;
-      const result: Result<never> = failure(newError);
-      const failures: SimpleFailure[] = [preExistingError];
+      const preExistingError = failure1
+      const newError = failure2
+      const result: Result<never> = failure(newError)
+      const failures: SimpleFailure[] = [preExistingError]
 
       // Act
-      const collectedValue = validateAndCollect(result, failures);
+      const collectedValue = validateAndCollect(result, failures)
 
       // Assert
-      expect(collectedValue).toBeNull();
-      expect(failures.length).toBe(2);
-      expect(failures).toContainEqual(preExistingError);
-      expect(failures).toContainEqual(newError);
-    });
+      expect(collectedValue).toBeNull()
+      expect(failures.length).toBe(2)
+      expect(failures).toContainEqual(preExistingError)
+      expect(failures).toContainEqual(newError)
+    })
 
-    it("deve retornar o valor e não modificar um array de falhas pré-existente quando o resultado é um sucesso", () => {
+    it('deve retornar o valor e não modificar um array de falhas pré-existente quando o resultado é um sucesso', () => {
       // Arrange
-      const successValue = "dados de sucesso";
-      const result: Result<string> = success(successValue);
-      const preExistingError = failure2;
-      const failures: SimpleFailure[] = [preExistingError];
+      const successValue = 'dados de sucesso'
+      const result: Result<string> = success(successValue)
+      const preExistingError = failure2
+      const failures: SimpleFailure[] = [preExistingError]
 
       // Act
-      const collectedValue = validateAndCollect(result, failures);
+      const collectedValue = validateAndCollect(result, failures)
 
       // Assert
-      expect(collectedValue).toBe(successValue);
-      expect(failures.length).toBe(1);
-      expect(failures).toContainEqual(preExistingError);
-    });
-  });
+      expect(collectedValue).toBe(successValue)
+      expect(failures.length).toBe(1)
+      expect(failures).toContainEqual(preExistingError)
+    })
+  })
 
-  describe("Common Validators", () => {
-    let failures: SimpleFailure[];
+  describe('parseToEnum', () => {
+    let failures: SimpleFailure[]
+    const fieldName = 'test_enum'
+
+    enum StringEnum {
+      ValueA = 'VALUE_A',
+      ValueB = 'VALUE_B',
+    }
 
     beforeEach(() => {
-      failures = [];
-    });
+      failures = []
+    })
 
-    describe("parseToEnum", () => {
-      enum StringEnum {
-        ValueA = "VALUE_A",
-        ValueB = "VALUE_B",
-      }
+    it('deve converter string para enum (IgnoreCase, sucesso)', () => {
+      // Arrange
+      const result = parseToEnum(fieldName, 'value_a', StringEnum, Mode.IGNORE_CASE)
 
-      it("deve converter string para enum (IgnoreCase, sucesso)", () => {
-        // Arrange
-        const result = parseToEnum("value_a", StringEnum, Mode.IGNORE_CASE);
+      // Act
+      const parsedValue = validateAndCollect(result, failures)
 
-        // Act
-        const parsedValue = validateAndCollect(result, failures);
+      // Assert
+      expect(result).toBeDefined()
+      expect(parsedValue).toBe(StringEnum.ValueA)
+      expect(failures).toHaveLength(0)
+    })
 
-        // Assert
-        expect(result).toBeDefined();
-        expect(parsedValue).toBe(StringEnum.ValueA);
-        expect(failures).toHaveLength(0);
-      });
+    it('deve converter string para enum (CaseSensitive, sucesso)', () => {
+      // Arrange
+      const result = parseToEnum(fieldName, 'VALUE_B', StringEnum, Mode.SENSITIVE_CASE)
 
-      it("deve converter string para enum (CaseSensitive, sucesso)", () => {
-        // Arrange
-        const result = parseToEnum("VALUE_B", StringEnum, Mode.SENSITIVE_CASE);
+      // Act
+      const parsedValue = validateAndCollect(result, failures)
 
-        // Act
-        const parsedValue = validateAndCollect(result, failures);
+      // Assert
+      expect(parsedValue).toBeDefined()
+      expect(parsedValue).toBe(StringEnum.ValueB)
+      expect(failures).toHaveLength(0)
+    })
 
-        // Assert
-        expect(parsedValue).toBeDefined();
-        expect(parsedValue).toBe(StringEnum.ValueB);
-        expect(failures).toHaveLength(0);
-      });
+    it('deve falhar ao converter string inexistente (IgnoreCase)', () => {
+      // Arrange
+      const result = parseToEnum(fieldName, 'non_existent', StringEnum, Mode.IGNORE_CASE)
 
-      it("deve falhar ao converter string inexistente (IgnoreCase)", () => {
-        // Arrange
-        const result = parseToEnum(
-          "non_existent",
-          StringEnum,
-          Mode.IGNORE_CASE,
-        );
+      // Act
+      const parsedValue = validateAndCollect(result, failures)
 
-        // Act
-        const parsedValue = validateAndCollect(result, failures);
+      // Assert
+      expect(parsedValue).toBeNull()
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE)
+    })
 
-        // Assert
-        expect(parsedValue).toBeNull();
-        expect(failures).toHaveLength(1);
-        expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE);
-        expect(failures[0].details).toEqual({
-          value: "NON_EXISTENT",
-          allowed_values: ["VALUE_A", "VALUE_B"],
-        });
-      });
+    it('deve falhar ao converter string inexistente (CaseSensitive)', () => {
+      // Arrange
+      const result = parseToEnum(fieldName, 'value_c', StringEnum, Mode.SENSITIVE_CASE)
 
-      it("deve falhar ao converter string inexistente (CaseSensitive)", () => {
-        // Arrange
-        const result = parseToEnum("value_c", StringEnum, Mode.SENSITIVE_CASE);
+      // Act
+      const parsedValue = validateAndCollect(result, failures)
 
-        // Act
-        const parsedValue = validateAndCollect(result, failures);
+      // Assert
+      expect(parsedValue).toBeNull()
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE)
+    })
 
-        // Assert
-        expect(parsedValue).toBeNull();
-        expect(failures).toHaveLength(1);
-        expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE);
-        expect(failures[0].details).toEqual({
-          value: "value_c",
-          allowed_values: ["VALUE_A", "VALUE_B"],
-        });
-      });
+    it('deve falhar ao converter string vazia', () => {
+      // Arrange
+      const result = parseToEnum(fieldName, '', StringEnum, Mode.IGNORE_CASE)
 
-      it("deve falhar ao converter string vazia", () => {
-        // Arrange
-        const result = parseToEnum("", StringEnum, Mode.IGNORE_CASE);
+      // Act
+      const parsedValue = validateAndCollect(result, failures)
 
-        // Act
-        const parsedValue = validateAndCollect(result, failures);
+      // Assert
+      expect(parsedValue).toBeNull()
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE)
+    })
 
-        // Assert
-        expect(parsedValue).toBeNull();
-        expect(failures).toHaveLength(1);
-        expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE);
-        expect(failures[0].details).toEqual({
-          value: "",
-          allowed_values: ["VALUE_A", "VALUE_B"],
-        });
-      });
+    it('deve falhar quando o valor for nulo', () => {
+      // Arrange
+      failures = []
 
-      it("deve falhar quando o valor for nulo", () => {
-        // Arrange
-        const values: any[] = [null, undefined];
+      // Act
+      const parsedValue = validateAndCollect(parseToEnum(fieldName, null, StringEnum, Mode.IGNORE_CASE), failures)
 
-        // Act
-        values.forEach((value) => {
-          failures = [];
-          const parsedValue = validateAndCollect(
-            parseToEnum(value, StringEnum, Mode.IGNORE_CASE),
-            failures,
-          );
+      // Assert
+      expect(parsedValue).toBeFalsy()
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE)
+    })
+  })
 
-          // Assert
-          expect(parsedValue).toBeFalsy();
-          expect(failures).toHaveLength(1);
-          expect(failures[0].code).toBe(FailureCode.INVALID_ENUM_VALUE);
-          expect(failures[0].details.allowed_values).toEqual([
-            "VALUE_A",
-            "VALUE_B",
-          ]);
-        });
-      });
-    });
-  });
-});
+  describe('hydrateEnum', () => {
+    enum TestEnum {
+      A = 'A',
+      B = 'B',
+    }
+
+    it('deve retornar o valor correto do enum quando válido', () => {
+      const value = hydrateEnum('campo', 'A', TestEnum)
+      expect(value).toBe(TestEnum.A)
+    })
+
+    it('deve lançar TechnicalError quando valor inválido', () => {
+      expect(() => hydrateEnum('campo', 'C', TestEnum)).toThrowError()
+    })
+
+    it('deve lançar TechnicalError quando valor for null', () => {
+      expect(() => hydrateEnum('campo', null, TestEnum)).toThrowError()
+    })
+
+    it('deve lançar TechnicalError quando valor for undefined', () => {
+      expect(() => hydrateEnum('campo', undefined, TestEnum)).toThrowError()
+    })
+  })
+})
--- src/shared/validator/common.validators.ts
+++ src/shared/validator/common.validators.ts
@@ -1,8 +1,9 @@
-import { SimpleFailure } from "../failure/simple.failure.type";
-import { FailureCode } from "../failure/failure.codes.enum";
-import { isNull } from "./validator";
-import { Result, success, failure } from "../result/result";
-import { TechnicalError } from "../error/technical.error";
+import { SimpleFailure } from '../failure/simple.failure.type'
+import { FailureCode } from '../failure/failure.codes.enum'
+import { isNull } from './validator'
+import { failure, Result, success } from '../result/result'
+import { TechnicalError } from '../error/technical.error'
+import { FailureFactory } from '../failure/failure.factory'
 
 /**
  * Verifica se valores são nulos ou indefinidos e cria falhas para cada campo inválido.
@@ -21,21 +22,19 @@ import { TechnicalError } from "../error/technical.error";
  *   return failure(failures);
  * }
  */
-export function ensureNotNull(
-  fieldsToCheck: Record<string, any>,
-): SimpleFailure[] {
-  const failures: SimpleFailure[] = [];
+export function ensureNotNull(fieldsToCheck: Record<string, any>): SimpleFailure[] {
+  const failures: SimpleFailure[] = []
 
   for (const [fieldName, fieldValue] of Object.entries(fieldsToCheck)) {
     if (isNull(fieldValue)) {
       failures.push({
         code: FailureCode.MISSING_REQUIRED_DATA,
         details: { field: fieldName },
-      });
+      })
     }
   }
 
-  return failures;
+  return failures
 }
 
 /**
@@ -57,18 +56,16 @@ export function ensureNotNull(
  *   throw new TechnicalError(FailureCode.MISSING_REQUIRED_DATA, { fields: nullFields });
  * }
  */
-export function collectNullFields(
-  fieldsToCheck: Record<string, any>,
-): string[] {
-  const nullFields: string[] = [];
+export function collectNullFields(fieldsToCheck: Record<string, any>): string[] {
+  const nullFields: string[] = []
 
   for (const [fieldName, fieldValue] of Object.entries(fieldsToCheck)) {
     if (isNull(fieldValue)) {
-      nullFields.push(fieldName);
+      nullFields.push(fieldName)
     }
   }
 
-  return nullFields;
+  return nullFields
 }
 
 /**
@@ -79,68 +76,62 @@ export function collectNullFields(
  * @param result O objeto Result a ser validado
  * @param failures Array onde as falhas serão coletadas, caso existam
  * @returns O valor contido no Result se for um sucesso, ou null se for uma falha
- */
-/**
- * Coleta o resultado de um Result e adiciona falhas a um array.
- * @param result O Result a ser processado.
- * @param failures O array de falhas onde os erros serão adicionados.
- * @returns O valor de sucesso do Result, ou null se for uma falha.
- */
-export function validateAndCollect<T>(
-  result: Result<T>,
-  failures: SimpleFailure[],
-): T | null {
+ **/
+export function validateAndCollect<T>(result: Result<T>, failures: SimpleFailure[]): T {
   if (result.isInvalid()) {
-    failures.push(...result.failures);
-    return null;
+    failures.push(...result.failures)
+    return null!
   } else {
-    return result.value;
+    return result.value
   }
 }
 
 export const enum Mode {
-  IGNORE_CASE = "IGNORE_CASE",
-  SENSITIVE_CASE = "SENSITIVE_CASE",
+  IGNORE_CASE = 'IGNORE_CASE',
+  SENSITIVE_CASE = 'SENSITIVE_CASE',
 }
 
 /**
  * Tenta converter uma string para um valor de enum.
  *
+ * @param fieldName Nome do campo que deve te rum valor correspondente ao enum
  * @param value A string a ser convertida.
  * @param enumType O tipo do enum.
  * @param mode O modo de comparação (IgnoreCase ou CaseSensitive).
  * @returns Um Result contendo o valor do enum em caso de sucesso, ou falhas caso a string não corresponda a nenhum valor do enum.
  */
 export function parseToEnum<T extends Record<string, string | number>>(
+  fieldName: string,
   value: string | null | undefined,
   enumType: T,
-  mode: Mode = Mode.IGNORE_CASE,
+  mode: Mode = Mode.IGNORE_CASE
 ): Result<T[keyof T]> {
-  TechnicalError.if(isNull(enumType), FailureCode.MISSING_REQUIRED_DATA, {
-    field: "enumType",
-  });
+  TechnicalError.validateRequiredFields({ enumType })
 
-  if (isNull(value))
-    return failure({
-      code: FailureCode.INVALID_ENUM_VALUE,
-      details: { value, allowed_values: Object.values(enumType).map(String) },
-    });
+  const enumValues = Object.values(enumType)
 
-  const stringValue =
-    mode === Mode.IGNORE_CASE ? String(value).toUpperCase() : String(value);
-  const enumValues = Object.values(enumType);
+  if (isNull(value)) return failure(FailureFactory.INVALID_ENUM_VALUE('N/D', fieldName))
+
+  const stringValue = mode === Mode.IGNORE_CASE ? String(value).toUpperCase() : String(value)
 
   const foundValue = enumValues.find((enumValue) => {
-    const enumString = String(enumValue);
-    return enumString === stringValue;
-  });
+    const enumString = String(enumValue)
+    return enumString === stringValue
+  })
 
-  if (isNull(foundValue)) {
-    return failure({
-      code: FailureCode.INVALID_ENUM_VALUE,
-      details: { value: stringValue, allowed_values: enumValues.map(String) },
-    });
+  if (isNull(foundValue)) return failure(FailureFactory.INVALID_ENUM_VALUE(value?.toString() || 'N/D', fieldName))
+
+  return success(foundValue as T[keyof T])
+}
+
+export function hydrateEnum<T extends Record<string, string | number>>(
+  fieldName: string,
+  value: string | null | undefined,
+  enumType: T
+): T[keyof T] {
+  const result = parseToEnum(fieldName, value, enumType, Mode.IGNORE_CASE)
+  if (result.isValid()) {
+    return result.value
   }
-
-  return success(foundValue as T[keyof T]);
+  throw new TechnicalError(result.failures[0])
 }
