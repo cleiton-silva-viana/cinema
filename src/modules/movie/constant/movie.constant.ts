6b40d08ebc1534ddd79d28608041a310f1cbd252
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-26 20:19:39 -0300

    feat(/movie/constat/): Adiciona constante `MOVIE_APPLICATION_SERVICE` para mecanismo de injeção de dependências.

--- src/modules/movie/constant/movie.constant.ts
+++ src/modules/movie/constant/movie.constant.ts
@@ -2,6 +2,5 @@
  * Chave de injeção para o repositório de filmes.
  * Utilizada para injeção de dependência no NestJS.
  */
-export const MOVIE_REPOSITORY = "MOVIE_REPOSITORY";
-export const DOMAIN_MOVIE_SERVICE = "DOMAIN_MOVIE_SERVICE";
-export const APPLICATION_MOVIE_SERVICE = "APPLICATION_MOVIE_SERVICE";
+export const MOVIE_REPOSITORY = 'MOVIE_REPOSITORY'
+export const MOVIE_APPLICATION_SERVICE = 'MOVIE_APPLICATION_SERVICE'
