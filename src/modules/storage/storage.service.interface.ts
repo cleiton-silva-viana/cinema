5d8f6e4317a66184d4723140b82f94be301bf0dd
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-25 22:52:04 -0300

    style(FilePaths): Aplica linter...

--- src/modules/storage/storage.service.interface.ts
+++ src/modules/storage/storage.service.interface.ts
@@ -1,13 +1,15 @@
-import { Result } from "../../shared/result/result";
+import { Result } from '@shared/result/result'
+import { ImageUID } from '@/modules/image/entity/value-object/image.uid'
 
 export interface FilePaths {
-  small: string;
-  normal: string;
-  large: string;
-  uid: string;
+  small: string
+  normal: string
+  large: string
+  uid: string
 }
 
 export interface IStorageService {
-  save(basePath: string, buffers: { small: Buffer; normal: Buffer; large: Buffer }): Promise<Result<FilePaths>>;
-  delete(uid: string): Promise<Result<null>>;
+  save(basePath: string, buffers: { small: Buffer; normal: Buffer; large: Buffer }): Promise<Result<FilePaths>>
+
+  delete(uid: ImageUID): Promise<Result<null>>
 }
