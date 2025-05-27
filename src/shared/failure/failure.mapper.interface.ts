012fed87429f84e8627fe95a411f4ac463d10871
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-27 12:10:32 -0300

    style(IFailureMapper): Aplica alterações na assinatura para incluir o idioma desejado usando o enum `SupportedLanguage`.

--- src/shared/failure/failure.mapper.interface.ts
+++ src/shared/failure/failure.mapper.interface.ts
@@ -1,8 +1,9 @@
 import { RichFailure } from './rich.failure.type'
 import { SimpleFailure } from './simple.failure.type'
+import { SupportedLanguage } from '@shared/value-object/multilingual-content'
 
 export interface IFailureMapper {
-  toRichFailure(failure: SimpleFailure, language?: 'pt' | 'en'): RichFailure
+  toRichFailure(failure: SimpleFailure, language: SupportedLanguage): RichFailure
 
-  toRichFailures(failures: SimpleFailure[], language?: 'pt' | 'en'): RichFailure[]
+  toRichFailures(failures: SimpleFailure[], language: SupportedLanguage): RichFailure[]
 }
