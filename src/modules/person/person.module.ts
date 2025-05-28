01f49935d33d191abb1abe83f883e25c3f8043db
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-27 21:43:35 -0300

    refactor(PersonModule): Atualiza o módulo para incluir novos tokens de injeção de dependências.

--- src/modules/person/person.module.ts
+++ src/modules/person/person.module.ts
@@ -1,11 +1,22 @@
-import { Module } from "@nestjs/common";
-import { PersonController } from "./controller/person.controller";
-import { PersonDomainService } from "./service/person.domain.service";
+import { Module } from '@nestjs/common'
+import { PersonController } from './controller/person.controller'
+import { PersonApplicationService } from './service/person.application.service'
+import { PERSON_APPLICATION_SERVICE } from './constant/person.constant'
 
 @Module({
   imports: [],
   controllers: [PersonController],
-  providers: [PersonDomainService],
-  exports: [PersonDomainService],
+  providers: [
+    {
+      provide: PERSON_APPLICATION_SERVICE,
+      useClass: PersonApplicationService,
+    },
+  ],
+  exports: [
+    {
+      provide: PERSON_APPLICATION_SERVICE,
+      useClass: PersonApplicationService,
+    },
+  ],
 })
 export class PersonModule {}
