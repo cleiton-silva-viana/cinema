b21dab7b88416bf67cc92a1b35d2de88fe35b3bf
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-28 18:26:18 -0300

    refactor(ICustomerApplicationService): Altera os métodos de atualização para retornar uma instância completa do recurso atualizado, não mais apenas um  `Partial`.

--- src/modules/customer/service/customer.application.service.interface.ts
+++ src/modules/customer/service/customer.application.service.interface.ts
@@ -19,11 +19,11 @@ export interface ICustomerApplicationService {
   findById(uid: string | CustomerUID): Promise<Result<Customer>>
   findByEmail(email: string | Email): Promise<Result<Customer>>
   create(props: ICreateCustomerProps): Promise<Result<Customer>>
-  updateCustomerEmail(customerUID: string, email: string): Promise<Result<Partial<Customer>>>
-  updateCustomerName(customerUID: string, newName: string): Promise<Result<Partial<Customer>>>
-  updateCustomerBirthDate(customerUID: string, birthDate: Date): Promise<Result<Partial<Customer>>>
-  assignCustomerCPF(customerUID: string, cpf: string): Promise<Result<Partial<Customer>>>
-  removeCustomerCPF(customerUID: string): Promise<Result<Partial<Customer>>>
-  removeCustomerStudentCard(customerUID: string): Promise<Result<Partial<Customer>>>
-  assignCustomerStudentCard(customerUID: string, studentCard: IStudentCardInput): Promise<Result<Partial<Customer>>>
+  updateCustomerEmail(customerUID: string, email: string): Promise<Result<Customer>>
+  updateCustomerName(customerUID: string, newName: string): Promise<Result<Customer>>
+  updateCustomerBirthDate(customerUID: string, birthDate: Date): Promise<Result<Customer>>
+  assignCustomerCPF(customerUID: string, cpf: string): Promise<Result<Customer>>
+  removeCustomerCPF(customerUID: string): Promise<Result<Customer>>
+  removeCustomerStudentCard(customerUID: string): Promise<Result<Customer>>
+  assignCustomerStudentCard(customerUID: string, studentCard: IStudentCardInput): Promise<Result<Customer>>
 }
