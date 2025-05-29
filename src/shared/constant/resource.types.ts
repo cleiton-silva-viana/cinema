7fc18eece1daeba6c96b4d04af4daf46e1473924
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-28 22:50:45 -0300

    feat(ResourceTypes): Corrije nome errado: `JSON_API_REPSONSE`.

--- src/shared/constant/resource.types.ts
+++ src/shared/constant/resource.types.ts
@@ -1,8 +1,10 @@
-export const ResourceTypes = {
-  MOVIE: 'MOVIE',
-  PERSON: 'PERSON',
-  CUSTOMER: 'CUSTOMER',
-  ROOM: 'ROOM',
-  IMAGE: 'IMAGE',
-  SCREENING: 'SCREENING',
+export enum ResourceTypes {
+  MOVIE = 'MOVIE',
+  PERSON = 'PERSON',
+  CUSTOMER = 'CUSTOMER',
+  ROOM = 'ROOM',
+  IMAGE = 'IMAGE',
+  SCREENING = 'SCREENING',
+
+  JSON_API_RESPONSE = 'JSON_API_RESPONSE',
 }
