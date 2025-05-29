20d15d01b75745d35d3d95a3e5d522a8d06a434a
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-29 13:41:35 -0300

    refactor(IRoomRepository): Altera o método `findById` para retornar o recurso desejado ou null.

--- src/modules/room/repository/room.repository.interface.ts
+++ src/modules/room/repository/room.repository.interface.ts
@@ -1,12 +1,18 @@
-import { Room } from "../entity/room";
-import { BookingSlot } from "../entity/value-object/booking.slot";
+import { Room } from '../entity/room'
+import { BookingSlot } from '../entity/value-object/booking.slot'
 
 export interface IRoomRepository {
-  roomExists(roomID: number): Promise<boolean>;
-  findById(roomID: number): Promise<Room>;
-  create(room: Room): Promise<Room>;
-  update(roomID: number, room: Partial<Room>): Promise<Room>;
-  delete(roomID: number): Promise<null>;
-  addBooking(roomID: number, booking: BookingSlot): Promise<Room>;
-  deleteBooking(roomID: number, bookingUID: string): Promise<null>;
+  roomExists(roomID: number): Promise<boolean>
+
+  findById(roomID: number): Promise<Room| null>
+
+  create(room: Room): Promise<Room>
+
+  update(roomID: number, room: Partial<Room>): Promise<Room>
+
+  delete(roomID: number): Promise<null>
+
+  addBooking(roomID: number, booking: BookingSlot): Promise<Room>
+
+  deleteBooking(roomID: number, bookingUID: string): Promise<null>
 }
