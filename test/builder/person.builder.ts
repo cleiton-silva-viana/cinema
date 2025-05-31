b87e12e3b7afc282f65d7eb6dec8b44074aaaa30
Cleiton Viana <estudo.cleiton@gmail.com>
2025-05-31 18:38:51 -0300

    feat(person.builder): Cria método que retorna um DTO de criação válido.

--- test/builder/person.builder.ts
+++ test/builder/person.builder.ts
@@ -1,21 +1,26 @@
-import {Person} from "@modules/person/entity/person";
-import {PersonUID} from "@modules/person/entity/value-object/person.uid";
-import {faker} from "@faker-js/faker";
+import { Person } from '@modules/person/entity/person'
+import { PersonUID } from '@modules/person/entity/value-object/person.uid'
+import { faker } from '@faker-js/faker'
+import { CreatePersonDTO } from '@modules/person/controller/dto/person.dto'
 
-export function CreateTestPerson(override?: Partial<{ uid: string, name: string, birthDate: Date }>) {
-    const uid = override?.uid ?? PersonUID.create().value
-    const name = override?.name ?? faker.person.firstName()
-    const birthDate = override?.birthDate ?? faker.date.birthdate()
+export function CreateTestPerson(override?: Partial<{ uid: string; name: string; birthDate: Date }>) {
+  const uid = override?.uid ?? PersonUID.create().value
+  const name = override?.name ?? faker.person.firstName()
+  const birthDate = override?.birthDate ?? faker.date.birthdate()
 
-    return Person.hydrate(uid, name, birthDate)
+  return Person.hydrate(uid, name, birthDate)
 }
 
-export function CloneTestPersonWithOverrides(person: Person, override: Partial<{ name: string, birthDate: Date }>) {
-    const {name, birthDate} = override
+export function CloneTestPersonWithOverrides(person: Person, override: Partial<{ name: string; birthDate: Date }>) {
+  const { name, birthDate } = override
 
-    return Person.hydrate(
-        person.uid.value,
-        name ?? person.name.value,
-        birthDate ?? person.birthDate.value
-    )
+  return Person.hydrate(person.uid.value, name ?? person.name.value, birthDate ?? person.birthDate.value)
+}
+
+export function CreateTestPersonDTO(override?: Partial<{ name: string; birthDate: Date }>) {
+  const dto: CreatePersonDTO = {
+    name: override?.name ?? faker.person.firstName(),
+    birthDate: override?.birthDate ?? faker.date.birthdate(),
+  }
+  return dto
 }
