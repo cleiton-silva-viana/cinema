a7aa4c7ca7b02d52f101e7c4e00d6eebc0b7091b
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-03 17:39:26 -0300

    feat(ILanguageContent): Define a interface que representa como deve ser a estrutura de um objeto `LanguageContent` após criado.

---
+++ src/shared/value-object/language-content/language.content.interface.ts
@@ -0,0 +1,13 @@
+import {SupportedLanguageEnum} from "@shared/value-object/language-content/supported.language.enum";
+
+/**
+ * Interface que define a estrutura de um conteúdo em um idioma específico
+ * Exemplo:
+ * ```ts
+ * { text: "Hello World", language: SupportedLanguageEnum.EN }
+ * ```
+ */
+export interface ILanguageContent {
+    text: string
+    language: SupportedLanguageEnum
+}
