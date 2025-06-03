16d388121fe07c01fa9c880c54a185dc4720c330
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-03 17:38:44 -0300

    feat(SupportedLanguageEnum): Cria arquivo específico contendo tal enum com os idiomas suportados pela aplicação.

---
+++ src/shared/value-object/language-content/supported.language.enum.ts
@@ -0,0 +1,8 @@
+/**
+ * Enum que representa os idiomas suportados pela aplicação.
+ * Cada membro corresponde a um código de idioma padrão (ISO 639-1).
+ */
+export enum SupportedLanguageEnum {
+  PT = 'PT',
+  EN = 'EN',
+}
