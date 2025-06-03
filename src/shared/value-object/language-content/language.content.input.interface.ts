fb46cbcd104d78e56e37b54fa9f7a7af33c2bbd8
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-03 17:37:49 -0300

    feat(ILanguageContentInput): Cria uma interface define os dados necessários para criarmos um objeto conteúdo textual multidioma.

---
+++ src/shared/value-object/language-content/language.content.input.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * Interface para entrada de conteúdo multilíngue
+ * Exemplo:
+ * ```ts
+ * { text: "Olá Mundo", language: "pt" }
+ * ```
+ */
+export interface ILanguageContentInput  {
+    text: string
+    language: string
+}
