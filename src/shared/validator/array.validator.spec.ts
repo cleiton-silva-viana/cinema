1c33e7c8d9569100a2998a9e277222cd81716cfb
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-04 09:50:29 -0300

    refactor(ArrayValidator): Refatora para utilizar funções que retornam uma falha para maior segurança nos tipos de erro retornados.

--- src/shared/validator/array.validator.spec.ts
+++ src/shared/validator/array.validator.spec.ts
@@ -1,67 +1,57 @@
 import { ArrayValidator } from './array.validator'
 import { FailureCode } from '../failure/failure.codes.enum'
 import { SimpleFailure } from '../failure/simple.failure.type'
+import { FailureFactory } from '@shared/failure/failure.factory'
 
 describe('ArrayValidator', () => {
+  let failures: SimpleFailure[]
+  const FAILURE = FailureFactory.MISSING_REQUIRED_DATA('data')
+
+  beforeEach(() => (failures = []))
+
   describe('isNotEmpty', () => {
     it('não deve adicionar falha quando o array não estiver vazio', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value = [1, 2, 3]
 
       // Act
       new ArrayValidator({ itens: value }, failures).isNotEmpty()
 
       // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(0)
     })
 
     it('deve adicionar falha quando o array estiver vazio', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value: number[] = []
 
       // Act
       new ArrayValidator({ itens: value }, failures).isNotEmpty()
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.MISSING_REQUIRED_DATA)
     })
 
-    it('deve usar o código de erro personalizado', () => {
+    it('deve usar o a falha personalizado', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value: number[] = []
-      const code = FailureCode.CONTENT_WITH_INVALID_TYPE
 
       // Act
-      new ArrayValidator({ itens: value }, failures).isNotEmpty(code)
+      new ArrayValidator({ itens: value }, failures).isNotEmpty(() => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
-    })
-
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value: number[] = []
-      const details = { message: 'Lista não pode estar vazia' }
-
-      // Act
-      new ArrayValidator({ itens: value }, failures).isNotEmpty(FailureCode.MISSING_REQUIRED_DATA, details)
-
-      // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details).toMatchObject(details)
+      expect(failures).toHaveLength(1)
+      expect(failures[0]).toBe(FAILURE)
     })
   })
 
   describe('hasLengthBetween', () => {
+    const MIN = 2
+    const MAX = 5
+
     it('não deve adicionar falha quando o tamanho estiver dentro do intervalo', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value = [1, 2, 3, 4]
       const min = 2
       const max = 5
@@ -70,208 +60,128 @@ describe('ArrayValidator', () => {
       new ArrayValidator({ itens: value }, failures).hasLengthBetween(min, max)
 
       // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(0)
     })
 
     it('deve adicionar falha quando o tamanho for menor que o mínimo', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value = [1]
-      const min = 2
-      const max = 5
 
       // Act
-      new ArrayValidator({ itens: value }, failures).hasLengthBetween(min, max)
+      new ArrayValidator({ itens: value }, failures).hasLengthBetween(MIN, MAX)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(FailureCode.STRING_LENGTH_OUT_OF_RANGE)
-      expect(failures[0].details?.min).toBe(min)
-      expect(failures[0].details?.max).toBe(max)
-      expect(failures[0].details?.count).toBe(value.length)
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.ARRAY_LENGTH_IS_OUT_OF_RANGE)
     })
 
     it('deve adicionar falha quando o tamanho for maior que o máximo', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value = [1, 2, 3, 4, 5, 6]
-      const min = 2
-      const max = 5
 
       // Act
-      new ArrayValidator({ itens: value }, failures).hasLengthBetween(min, max)
+      new ArrayValidator({ itens: value }, failures).hasLengthBetween(MIN, MAX)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(FailureCode.STRING_LENGTH_OUT_OF_RANGE)
-      expect(failures[0].details?.min).toBe(min)
-      expect(failures[0].details?.max).toBe(max)
-      expect(failures[0].details?.count).toBe(value.length)
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.ARRAY_LENGTH_IS_OUT_OF_RANGE)
     })
 
-    it('deve usar o código de erro personalizado', () => {
+    it('deve usar uma falha personalizado', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value = [1]
-      const min = 2
-      const max = 5
-      const code = FailureCode.CONTENT_WITH_INVALID_TYPE
 
       // Act
-      new ArrayValidator({ itens: value }, failures).hasLengthBetween(min, max, code)
+      new ArrayValidator({ itens: value }, failures).hasLengthBetween(MIN, MAX, () => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
-    })
-
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [1]
-      const min = 2
-      const max = 5
-      const details = { message: 'Quantidade de itens inválida' }
-
-      // Act
-      new ArrayValidator({ itens: value }, failures).hasLengthBetween(
-        min,
-        max,
-        FailureCode.STRING_LENGTH_OUT_OF_RANGE,
-        details
-      )
-
-      // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details?.message).toBe(details.message)
-      expect(failures[0].details?.min).toBe(min)
-      expect(failures[0].details?.max).toBe(max)
-      expect(failures[0].details?.count).toBe(value.length)
+      expect(failures).toHaveLength(1)
+      expect(failures[0]).toEqual(FAILURE)
     })
   })
 
   describe('contains', () => {
+    const ITEMS = [1, 2, 3]
+
     it('não deve adicionar falha quando o array contém o item', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const item = 2
-      const value = [1, 2, 3]
 
       // Act
-      new ArrayValidator({ itens: value }, failures).contains(item)
+      new ArrayValidator({ ITEMS }, failures).contains(item)
 
       // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(0)
     })
 
     it('deve adicionar falha quando o array não contém o item', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const item = 5
-      const value = [1, 2, 3]
 
       // Act
-      new ArrayValidator({ itens: value }, failures).contains(item)
+      new ArrayValidator({ ITEMS }, failures).contains(item)
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.MISSING_REQUIRED_DATA)
     })
 
     it('deve usar o código de erro personalizado', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const item = 5
-      const value = [1, 2, 3]
-      const code = FailureCode.CONTENT_WITH_INVALID_TYPE
 
       // Act
-      new ArrayValidator({ itens: value }, failures).contains(item, code)
+      new ArrayValidator({ ITEMS }, failures).contains(item, () => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
-    })
-
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const item = 5
-      const value = [1, 2, 3]
-      const details = { message: 'Item obrigatório não encontrado' }
-
-      // Act
-      new ArrayValidator({ itens: value }, failures).contains(item, FailureCode.MISSING_REQUIRED_DATA, details)
-
-      // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details?.message).toBe(details.message)
+      expect(failures).toHaveLength(1)
+      expect(failures[0]).toEqual(FAILURE)
     })
   })
 
   describe('every', () => {
+    const ITEMS = [2, 4, 6, 8]
     it('não deve adicionar falha quando todos os itens satisfazem a condição', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [2, 4, 6, 8]
       const predicate = (item: number) => item % 2 === 0
 
       // Act
-      new ArrayValidator({ itens: value }, failures).every(predicate)
+      new ArrayValidator({ ITEMS }, failures).every(predicate)
 
       // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(0)
     })
 
     it('deve adicionar falha quando algum item não satisfaz a condição', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [2, 4, 5, 8]
+      const testItems = [...ITEMS, 5]
       const predicate = (item: number) => item % 2 === 0
 
       // Act
-      new ArrayValidator({ itens: value }, failures).every(predicate)
+      new ArrayValidator({ testItems }, failures).every(predicate)
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.CONTENT_WITH_INVALID_ITEMS)
     })
 
-    it('deve usar o código de erro personalizado', () => {
+    it('deve usar a falha personalizada', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [2, 4, 5, 8]
+      const testItems = [...ITEMS, 5]
       const predicate = (item: number) => item % 2 === 0
-      const code = FailureCode.CONTENT_WITH_INVALID_FORMAT
 
       // Act
-      new ArrayValidator({ itens: value }, failures).every(predicate, code)
+      new ArrayValidator({ testItems }, failures).every(predicate, () => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
-    })
-
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [2, 4, 5, 8]
-      const predicate = (item: number) => item % 2 === 0
-      const details = { message: 'Todos os itens devem ser pares' }
-
-      // Act
-      new ArrayValidator({ itens: value }, failures).every(predicate, FailureCode.CONTENT_WITH_INVALID_ITEMS, details)
-
-      // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details).toMatchObject(details)
+      expect(failures).toHaveLength(1)
+      expect(failures[0]).toEqual(FAILURE)
     })
   })
 
   describe('some', () => {
     it('não deve adicionar falha quando pelo menos um item satisfaz a condição', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
       const value = [1, 3, 5, 8]
       const predicate = (item: number) => item % 2 === 0
 
@@ -279,51 +189,33 @@ describe('ArrayValidator', () => {
       new ArrayValidator({ itens: value }, failures).some(predicate)
 
       // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(0)
     })
+
+    const ITEMS = [3, 5, 7]
 
     it('deve adicionar falha quando nenhum item satisfaz a condição', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [1, 3, 5, 7]
       const predicate = (item: number) => item % 2 === 0
 
       // Act
-      new ArrayValidator({ itens: value }, failures).some(predicate)
+      new ArrayValidator({ ITEMS }, failures).some(predicate)
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.MISSING_VALID_ITEM)
     })
 
     it('deve usar o código de erro personalizado', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [1, 3, 5, 7]
       const predicate = (item: number) => item % 2 === 0
-      const code = FailureCode.CONTENT_WITH_INVALID_FORMAT
 
       // Act
-      new ArrayValidator({ itens: value }, failures).some(predicate, code)
+      new ArrayValidator({ ITEMS }, failures).some(predicate, () => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
-    })
-
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value = [1, 3, 5, 7]
-      const predicate = (item: number) => item % 2 === 0
-      const details = { message: 'Pelo menos um item deve ser par' }
-
-      // Act
-      new ArrayValidator({ itens: value }, failures).some(predicate, FailureCode.MISSING_VALID_ITEM, details)
-
-      // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details).toMatchObject(details)
+      expect(failures).toHaveLength(1)
+      expect(failures[0]).toEqual(FAILURE)
     })
   })
 })
--- src/shared/validator/array.validator.ts
+++ src/shared/validator/array.validator.ts
@@ -1,105 +1,96 @@
-import { BaseValidator } from './base.validator.ts'
-import { FailureCode } from '../failure/failure.codes.enum'
 import { SimpleFailure } from '../failure/simple.failure.type'
+import { FailureFactory } from '../failure/failure.factory'
+import { AbstractValidator } from './abstract.validator'
 
 /**
- * Validador para arrays
+ * `ArrayValidator` é uma classe utilitária para validar arrays, estendendo `AbstractValidator`.
+ * Fornece métodos para verificar a não-vacuidade, obrigatoriedade, tamanho, conteúdo e conformidade de elementos do array.
+ * @template T O tipo dos elementos no array.
  */
-export class ArrayValidator<T> extends BaseValidator<ArrayValidator<T>> {
+export class ArrayValidator<T> extends AbstractValidator<ArrayValidator<T>> {
+  protected _field: string
   constructor(value: Record<string, Array<T>>, failures: SimpleFailure[] = []) {
     super(value, failures)
+    this._field = Object.keys(value)[0]
   }
 
   /**
-   * Verifica se o array não está vazio
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se o array não está vazio.
+   * @param failure Uma função opcional que retorna um `SimpleFailure` personalizado se a validação falhar.
+   * @returns A instância atual de `ArrayValidator` para encadeamento de chamadas.
    */
-  public isNotEmpty(
-    code: FailureCode = FailureCode.MISSING_REQUIRED_DATA,
-    details: Record<string, any> = {}
-  ): ArrayValidator<T> {
-    return this.validate(() => !(this._value.length > 0), {
-      code,
-      details,
-    })
+  public isNotEmpty(failure?: () => SimpleFailure): ArrayValidator<T> {
+    return this.validate(
+      () => !(this._value.length > 0),
+      failure ? failure() : FailureFactory.MISSING_REQUIRED_DATA(this._field)
+    )
   }
 
   /**
-   * Verifica se o tamanho do array está dentro do intervalo especificado
-   * @param min Tamanho mínimo (opcional)
-   * @param max Tamanho máximo (opcional)
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se o array não é nulo ou indefinido.
+   * @param failure Uma função opcional que retorna um `SimpleFailure` personalizado se a validação falhar.
+   * @returns A instância atual de `ArrayValidator` para encadeamento de chamadas.
    */
-  public hasLengthBetween(
-    min: number,
-    max: number,
-    code: FailureCode = FailureCode.STRING_LENGTH_OUT_OF_RANGE,
-    details: Record<string, any> = {}
-  ): ArrayValidator<T> {
+  public isRequired(failure?: () => SimpleFailure): ArrayValidator<T> {
+    return this.validate(
+      () => this._value == null,
+      failure ? failure() : FailureFactory.MISSING_REQUIRED_DATA(this._field)
+    )
+  }
+
+  /**
+   * Verifica se o tamanho do array está dentro do intervalo especificado.
+   * @param min O tamanho mínimo permitido para o array.
+   * @param max O tamanho máximo permitido para o array.
+   * @param failure Uma função opcional que retorna um `SimpleFailure` personalizado se a validação falhar.
+   * @returns A instância atual de `ArrayValidator` para encadeamento de chamadas.
+   */
+  public hasLengthBetween(min: number, max: number, failure?: () => SimpleFailure): ArrayValidator<T> {
     return this.validate(
       () => {
         const length = this._value.length
         return !(length >= min && length <= max)
       },
-      {
-        code,
-        details: {
-          min: min,
-          max: max,
-          count: this._value.length,
-          ...details,
-        },
-      }
+      failure ? failure() : FailureFactory.ARRAY_LENGTH_IS_OUT_OF_RANGE(this._field, min, max)
     )
   }
 
   /**
-   * Verifica se o array contém o item especificado
-   * @param item Item a ser verificado
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se o array contém o item especificado.
+   * @param item O item a ser verificado no array.
+   * @param failure Uma função opcional que retorna um `SimpleFailure` personalizado se a validação falhar.
+   * @returns A instância atual de `ArrayValidator` para encadeamento de chamadas.
    */
-  public contains(
-    item: T,
-    code: FailureCode = FailureCode.MISSING_REQUIRED_DATA,
-    details: Record<string, any> = {}
-  ): ArrayValidator<T> {
-    return this.validate(() => !this._value.includes(item), {
-      code,
-      details,
-    })
+  public contains(item: T, failure?: () => SimpleFailure): ArrayValidator<T> {
+    return this.validate(
+      () => !this._value.includes(item),
+      failure ? failure() : FailureFactory.MISSING_REQUIRED_DATA(this._field)
+    )
   }
 
   /**
-   * Verifica se todos os itens do array satisfazem a condição
-   * @param predicate Função que verifica cada item
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se todos os itens do array satisfazem a condição fornecida.
+   * @param predicate Uma função que testa cada elemento do array. Retorna `true` para um elemento que passa no teste, `false` caso contrário.
+   * @param failure Uma função opcional que retorna um `SimpleFailure` personalizado se a validação falhar.
+   * @returns A instância atual de `ArrayValidator` para encadeamento de chamadas.
    */
-  public every(
-    predicate: (item: T) => boolean,
-    code: FailureCode = FailureCode.CONTENT_WITH_INVALID_ITEMS,
-    details: Record<string, any> = {}
-  ): ArrayValidator<T> {
-    return this.validate(() => !this._value.every(predicate), {
-      code,
-      details,
-    })
+  public every(predicate: (item: T) => boolean, failure?: () => SimpleFailure): ArrayValidator<T> {
+    return this.validate(
+      () => !this._value.every(predicate),
+      failure ? failure() : FailureFactory.CONTENT_WITH_INVALID_ITEMS(this._field)
+    )
   }
 
   /**
-   * Verifica se pelo menos um item do array satisfaz a condição
-   * @param predicate Função que verifica cada item
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se pelo menos um item do array satisfaz a condição fornecida.
+   * @param predicate Uma função que testa cada elemento do array. Retorna `true` para um elemento que passa no teste, `false` caso contrário.
+   * @param failure Uma função opcional que retorna um `SimpleFailure` personalizado se a validação falhar.
+   * @returns A instância atual de `ArrayValidator` para encadeamento de chamadas.
    */
-  public some(
-    predicate: (item: T) => boolean,
-    code: FailureCode = FailureCode.MISSING_VALID_ITEM,
-    details: Record<string, any> = {}
-  ): ArrayValidator<T> {
-    return this.validate(() => !this._value.some(predicate), { code, details })
+  public some(predicate: (item: T) => boolean, failure?: () => SimpleFailure): ArrayValidator<T> {
+    return this.validate(
+      () => !this._value.some(predicate),
+      failure ? failure() : FailureFactory.MISSING_VALID_ITEM(this._field)
+    )
   }
 }
