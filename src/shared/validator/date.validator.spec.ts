8594eab815f4d434a954c021de1f9ea52880774e
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-05 11:05:04 -0300

    refactor(DateValidator): Atualiza a implementação e os testes para utilizar nova forma de usar falhas personalizadas.

--- src/shared/validator/date.validator.spec.ts
+++ src/shared/validator/date.validator.spec.ts
@@ -1,4 +1,5 @@
 import { FailureCode } from '../failure/failure.codes.enum'
+import { FailureFactory } from '../failure/failure.factory'
 import { SimpleFailure } from '../failure/simple.failure.type'
 import { DateValidator } from './date.validator'
 
@@ -9,9 +10,11 @@ describe('DateValidator', () => {
 
   const dataBefore = new Date(startDate.getTime() - ONE_DAY)
   const dataMiddle = new Date(startDate.getTime() + ONE_DAY)
-  const dataAfter = new Date(endDate.getTime() + ONE_DAY)
+  const FAILURE = FailureFactory.MISSING_VALID_ITEM('item')
 
-  const commonDetails = { message: 'Mensagem de erro personalizada' }
+  let failures: SimpleFailure[]
+
+  beforeEach(() => (failures = []))
 
   describe('Construtor e inicialização', () => {
     it('deve criar uma instância válida com uma data', () => {
@@ -42,54 +45,29 @@ describe('DateValidator', () => {
 
   describe('isAfter', () => {
     it('não deve adicionar falha quando a data for posterior à data limite', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
       // Act
       new DateValidator({ data: dataMiddle }, failures).isAfter(startDate)
 
       // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(0)
     })
 
     it('deve adicionar falha quando a data não for posterior à data limite', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
       // Act
       new DateValidator({ data: startDate }, failures).isAfter(endDate)
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.DATE_NOT_AFTER_LIMIT)
-      expect(failures[0].details?.date).toBe(startDate.toISOString())
-      expect(failures[0].details?.max_date).toBe(endDate.toISOString())
     })
 
-    it('deve usar o código de erro personalizado', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const code = FailureCode.CONTENT_WITH_INVALID_TYPE
-
+    it('deve usar a falha personalizada', () => {
       // Act
-      new DateValidator({ data: startDate }, failures).isAfter(endDate, code)
+      new DateValidator({ data: startDate }, failures).isAfter(endDate, () => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
-    })
-
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
-      // Act
-      new DateValidator({ data: startDate }, failures).isAfter(endDate, FailureCode.DATE_NOT_AFTER_LIMIT, commonDetails)
-
-      // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details?.date).toBe(startDate.toISOString())
-      expect(failures[0].details?.max_date).toBe(endDate.toISOString())
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FAILURE.code)
     })
 
     it('deve lidar com limit date inválido', () => {
@@ -100,349 +78,171 @@ describe('DateValidator', () => {
       for (const date of invalidDate) {
         expect(() => {
           new DateValidator({ data: startDate }, []).isAfter(date)
-        }).toThrow()
+        }).toThrowTechnicalError()
       }
     })
 
-    it('deve adicionar falha quando a data for um valor nulo ou undefined', () => {
-      // Arrange
-      const invalidValues: any[] = [null, undefined]
+    it('deve adicionar falha quando a data for um valor falsy', () => {
+      // Act
+      new DateValidator({ data: null as unknown as Date }, failures).isAfter(endDate)
 
-      for (const value of invalidValues) {
-        // Arrange
-        const failures: SimpleFailure[] = []
-
-        // Act
-        new DateValidator({ data: value }, failures).isAfter(endDate)
-
-        // Assert
-        expect(failures.length).toBe(1)
-        expect(failures[0].code).toBe(FailureCode.DATE_NOT_AFTER_LIMIT)
-      }
+      // Assert
+      expect(failures.length).toBeGreaterThan(0)
+      expect(failures[0].code).toBe(FailureCode.DATE_NOT_AFTER_LIMIT)
     })
   })
 
   describe('isBefore', () => {
     it('não deve adicionar falha quando a data for anterior à data limite', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
       // Act
       new DateValidator({ data: startDate }, failures).isBefore(endDate)
 
       // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(0)
     })
 
     it('deve adicionar falha quando a data não for anterior à data limite', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
       // Act
       new DateValidator({ data: endDate }, failures).isBefore(startDate)
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
-      expect(failures[0].details?.date).toBe(endDate.toISOString())
-      expect(failures[0].details?.min_date).toBe(startDate.toISOString())
     })
 
-    it('deve adicionar falha quando a data for um valor nulo ou undefined', () => {
-      // Arrange
-      const invalidDates: any[] = [null, undefined]
-
-      for (const date of invalidDates) {
-        const failures: SimpleFailure[] = []
-
-        // Act
-        new DateValidator({ data: date }, failures).isBefore(endDate)
-
-        // Assert
-        expect(failures.length).toBe(1)
-        expect(failures[0].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
-      }
-    })
-
-    it('deve usar o código de erro personalizado', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const code = FailureCode.CONTENT_WITH_INVALID_FORMAT
-
+    it('deve adicionar falha quando a data for um valor valor falsy', () => {
       // Act
-      new DateValidator({ data: endDate }, failures).isBefore(startDate, code)
+      new DateValidator({ data: null as unknown as Date }, failures).isBefore(endDate)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
     })
 
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
+    it('deve usar uma falha personalizado', () => {
       // Act
-      new DateValidator({ data: endDate }, failures).isBefore(
-        startDate,
-        FailureCode.DATE_NOT_BEFORE_LIMIT,
-        commonDetails
-      )
+      new DateValidator({ data: endDate }, failures).isBefore(startDate, () => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details?.date).toBe(endDate.toISOString())
-      expect(failures[0].details?.min_date).toBe(startDate.toISOString())
+      expect(failures).toHaveLength(1)
+      expect(failures[0]).toEqual(FAILURE)
     })
 
     it('deve lidar com limitDate inválido', () => {
       // Arrange
-      const invalidLimits: any[] = [null, undefined, new Date('Invalid Date')]
+      const invalidLimits: any[] = [null, undefined]
 
-      for (const date of invalidLimits) {
-        // Assert
+      for (const limit of invalidLimits) {
         expect(() => {
-          new DateValidator({ data: startDate }, []).isBefore(date)
-        }).toThrow()
+          new DateValidator({ data: startDate }, []).isBefore(limit)
+        }).toThrowTechnicalError()
       }
     })
   })
 
   describe('isBetween', () => {
-    describe('cenários onde não deve ocorrer falha', () => {
-      const successCases = [
-        {
-          scenario: 'não deve adicionar falha quando a data estiver dentro do intervalo',
-          date: dataMiddle,
-        },
-        {
-          scenario: 'não deve adicionar falha quando a data for igual à data de início',
-          date: new Date(startDate),
-        },
-        {
-          scenario: 'não deve adicionar falha quando a data for igual à data de fim',
-          date: new Date(endDate),
-        },
-      ]
-
-      successCases.forEach(({ scenario, date }) => {
-        it(scenario, () => {
-          // Arrange
-          const failures: SimpleFailure[] = []
-
-          // Act
-          new DateValidator({ data: date }, failures).isBetween(startDate, endDate)
-
-          // Assert
-          expect(failures.length).toBe(0)
-        })
-      })
-    })
-
-    describe('cenários onde deve ocorrer uma falha', () => {
-      const failureCases = [
-        {
-          scenario: 'deve adicionar falha quando a data for anterior à data de início',
-          date: dataBefore,
-        },
-        {
-          scenario: 'deve adicionar falha quando a data for posterior à data de fim',
-          date: dataAfter,
-        },
-      ]
-
-      failureCases.forEach(({ scenario, date }) => {
-        // Arrange
-        it(scenario, () => {
-          const failures: SimpleFailure[] = []
-
-          // Act
-          new DateValidator({ data: date }, failures).isBetween(startDate, endDate)
-
-          // Assert
-          expect(failures.length).toBe(1)
-          expect(failures[0].code).toBe(FailureCode.DATE_OUT_OF_RANGE)
-          expect(failures[0].details?.date).toBe(date.toISOString())
-          expect(failures[0].details?.start_date).toBe(startDate.toISOString())
-          expect(failures[0].details?.end_date).toBe(endDate.toISOString())
-        })
-      })
-    })
-
-    it('deve usar o código de erro personalizado', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const code = FailureCode.CONTENT_WITH_INVALID_TYPE
-
+    it('não deve adicionar falha quando a data estiver entre as datas de início e fim', () => {
       // Act
-      new DateValidator({ data: dataAfter }, failures).isBetween(startDate, endDate, code)
+      new DateValidator({ data: dataMiddle }, failures).isBetween(startDate, endDate)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(code)
+      expect(failures).toHaveLength(0)
     })
 
-    it('deve incluir detalhes adicionais na falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
+    it('deve adicionar falha quando a data não estiver entre as datas de início e fim', () => {
       // Act
-      new DateValidator({ data: dataAfter }, failures).isBetween(
-        startDate,
-        endDate,
-        FailureCode.DATE_OUT_OF_RANGE,
-        commonDetails
-      )
+      new DateValidator({ data: dataBefore }, failures).isBetween(startDate, endDate)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].details?.date).toBe(dataAfter.toISOString())
-      expect(failures[0].details?.start_date).toBe(startDate.toISOString())
-      expect(failures[0].details?.end_date).toBe(endDate.toISOString())
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.DATE_IS_OUT_OF_RANGE)
     })
 
-    it('deve lidar com startDate inválido', () => {
-      // Arrange
-      const invalidDates: any[] = [null, undefined]
-
-      for (const date of invalidDates) {
-        const failures: SimpleFailure[] = []
-
-        // Act & Assert
-        expect(() => {
-          new DateValidator({ data: dataMiddle }, failures).isBetween(date, endDate)
-        }).toThrow()
-      }
-    })
-
-    it('deve lidar com endDate inválido', () => {
-      // Arrange
-      const invalidDates: any[] = [null, undefined]
-
-      for (const date of invalidDates) {
-        const failures: SimpleFailure[] = []
-
-        // Act & Assert
-        expect(() => {
-          new DateValidator({ data: dataMiddle }, failures).isBetween(startDate, date)
-        }).toThrow()
-      }
-    })
-
-    it('deve lidar com startDate e endDate invertidos', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
+    it('deve adicionar falha quando a data de início for posterior à data de fim', () => {
       // Act
       new DateValidator({ data: dataMiddle }, failures).isBetween(endDate, startDate)
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.DATE_WITH_INVALID_SEQUENCE)
     })
-  })
 
-  describe('Validações encadeadas', () => {
-    it('deve parar a validação após a primeira falha', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-
+    it('deve usar uma falha personalizado', () => {
       // Act
-      new DateValidator({ data: dataAfter }, failures)
-        .isBefore(startDate) // Falha aqui
-        .isAfter(endDate) // Não deve ser executado
+      new DateValidator({ data: dataBefore }, failures).isBetween(startDate, endDate, () => FAILURE)
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
+      expect(failures).toHaveLength(1)
+      expect(failures[0]).toEqual(FAILURE)
     })
 
-    it('deve continuar a validação quando continue for chamado', () => {
+    it('deve lidar com startDate ou endDate inválidos', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
-
-      // Act
-      new DateValidator({ data: dataAfter }, failures)
-        .isBefore(startDate) // Falha aqui
-        .continue()
-        .isAfter(startDate) // Deve ser executado
+      const invalidDates: any[] = [null, undefined]
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
+      for (const invalidDate of invalidDates) {
+        expect(() =>
+          new DateValidator({ data: dataMiddle }, []).isBetween(invalidDate, endDate)
+        ).toThrowTechnicalError()
+        expect(() =>
+          new DateValidator({ data: dataMiddle }, []).isBetween(startDate, invalidDate)
+        ).toThrowTechnicalError()
+      }
     })
 
-    it('deve acumular múltiplas falhas quando continue for chamado', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value = dataMiddle
-      const beforeDate = new Date(value.getTime() - ONE_DAY)
-      const afterDate = new Date(value.getTime() + ONE_DAY)
-
+    it('deve adicionar falha quando a data for um valor falsy', () => {
       // Act
-      new DateValidator({ data: value }, failures)
-        .isBefore(beforeDate) // Falha aqui
-        .continue()
-        .isAfter(afterDate) // Falha aqui também
+      new DateValidator({ data: null as unknown as Date }, failures).isBetween(startDate, endDate)
 
       // Assert
-      expect(failures.length).toBe(2)
-      expect(failures[0].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
-      expect(failures[1].code).toBe(FailureCode.DATE_NOT_AFTER_LIMIT)
-    })
-
-    it('deve executar validações condicionais com if', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value = dataMiddle
-
-      // Act
-      new DateValidator({ data: value }, failures)
-        .if(value.getMonth() === startDate.getMonth()) // Janeiro (true)
-        .isBefore(endDate) // Não falha
-        .if(value.getFullYear() !== startDate.getFullYear()) // Falso
-        .isAfter(endDate) // Não deve ser executado
-
-      // Assert
-      expect(failures.length).toBe(0)
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.CONTENT_WITH_INVALID_TYPE)
     })
   })
 
-  describe('Casos extremos', () => {
-    it('deve lidar com datas muito antigas', () => {
+  describe('encadeamento de validações', () => {
+    it('deve acumular falhas quando múltiplas validações falham', () => {
       // Arrange
-      const failures: SimpleFailure[] = []
-      const value = new Date('1900-01-01')
+      const value = new Date('2023-01-01')
 
       // Act
-      new DateValidator({ data: value }, failures).isAfter(startDate)
+      new DateValidator({ data: value }, failures)
+        .isAfter(new Date('2023-02-01'))
+        .continue()
+        .isBefore(new Date('2022-12-01'))
+        .continue()
+        .isBetween(new Date('2023-02-01'), new Date('2023-02-28'))
 
       // Assert
-      expect(failures.length).toBe(1)
+      expect(failures).toHaveLength(3)
+      expect(failures[0].code).toBe(FailureCode.DATE_NOT_AFTER_LIMIT)
+      expect(failures[1].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
+      expect(failures[2].code).toBe(FailureCode.DATE_IS_OUT_OF_RANGE)
+    })
+
+    it('deve parar de validar após o primeiro erro quando stopOnFirstFailure é usado', () => {
+      // Arrange
+      const value = new Date('2023-01-01')
+
+      // Act
+      new DateValidator({ data: value }, failures).isAfter(new Date('2023-02-01')).isBefore(new Date('2022-12-01'))
+
+      // Assert
+      expect(failures).toHaveLength(1)
       expect(failures[0].code).toBe(FailureCode.DATE_NOT_AFTER_LIMIT)
     })
 
-    it('deve lidar com datas muito futuras', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value = new Date('2100-01-01')
-
+    it('não deve lançar um erro quando um valor nulo é passado para um fluxo de validação', () => {
       // Act
-      new DateValidator({ data: value }, failures).isBefore(startDate)
+      new DateValidator({ data: null as unknown as Date }, failures)
+        .isAfter(new Date())
+        .isBefore(new Date())
+        .isBetween(new Date(), new Date())
 
       // Assert
-      expect(failures.length).toBe(1)
-      expect(failures[0].code).toBe(FailureCode.DATE_NOT_BEFORE_LIMIT)
-    })
-
-    it('deve lidar com datas inválidas', () => {
-      // Arrange
-      const failures: SimpleFailure[] = []
-      const value = new Date('Invalid Date')
-
-      // Act & Assert
-      expect(() => {
-        new DateValidator({ data: value }, failures).isAfter(startDate)
-      }).not.toThrow()
+      expect(failures).toHaveLength(1)
+      expect(failures[0].code).toBe(FailureCode.DATE_NOT_AFTER_LIMIT)
     })
   })
 })
--- src/shared/validator/date.validator.ts
+++ src/shared/validator/date.validator.ts
@@ -1,110 +1,92 @@
-import { BaseValidator } from './base.validator.ts'
+import { FailureFactory } from '../failure/failure.factory'
 import { FailureCode } from '../failure/failure.codes.enum'
 import { TechnicalError } from '../error/technical.error'
 import { SimpleFailure } from '../failure/simple.failure.type'
-import { isNull } from '@/shared/validator/validator'
+import { AbstractValidator } from './abstract.validator'
 
 /**
- * Validador para datas
+ * Validador para operações com datas.
+ * Estende `AbstractValidator` para fornecer métodos de validação específicos para objetos `Date`.
  */
-export class DateValidator extends BaseValidator<DateValidator> {
+export class DateValidator extends AbstractValidator<DateValidator> {
+  /**
+   * Cria uma nova instância de `DateValidator`.
+   * @param value O objeto `Record<string, Date>` contendo a data a ser validada.
+   * @param failures Um array de `SimpleFailure` para acumular erros de validação.
+   */
   constructor(value: Record<string, Date>, failures: SimpleFailure[]) {
     super(value, failures)
   }
 
   /**
-   * Verifica se a data é posterior à data limite
-   * @param limitDate Data limite para comparação
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se a data atual é posterior a uma data limite especificada.
+   * @param limitDate A data limite para comparação.
+   * @param failure (Opcional) Uma função que retorna um `SimpleFailure` customizado em caso de falha.
+   * @returns A instância atual de `DateValidator` para encadeamento de chamadas.
    */
-  public isAfter(
-    limitDate: Date,
-    code: FailureCode = FailureCode.DATE_NOT_AFTER_LIMIT,
-    details: Record<string, any> = {}
-  ): DateValidator {
+  public isAfter(limitDate: Date, failure?: () => SimpleFailure): DateValidator {
     TechnicalError.validateRequiredFields({ limitDate })
 
     const isValid = this._value instanceof Date && !isNaN(this._value.getTime())
 
     const date = isValid ? this._value : new Date(new Date(limitDate).setDate(limitDate.getDate() - 10000))
 
-    return this.validate(() => date < limitDate, {
-      code,
-      details: {
-        date: isValid ? date.toISOString() : 'N/A',
-        max_date: limitDate.toISOString(),
-        ...details,
-      },
-    })
+    return this.validate(
+      () => date < limitDate,
+      failure ? failure() : FailureFactory.DATE_NOT_AFTER_LIMIT(this._value, limitDate.toISOString())
+    )
   }
 
   /**
-   * Verifica se a data é anterior à data limite
-   * @param limitDate Data limite para comparação
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se a data atual é anterior a uma data limite especificada.
+   * @param limitDate A data limite para comparação.
+   * @param failure (Opcional) Uma função que retorna um `SimpleFailure` customizado em caso de falha.
+   * @returns A instância atual de `DateValidator` para encadeamento de chamadas.
    */
-  public isBefore(
-    limitDate: Date,
-    code: FailureCode = FailureCode.DATE_NOT_BEFORE_LIMIT,
-    details: Record<string, any> = {}
-  ): DateValidator {
+  public isBefore(limitDate: Date, failure?: () => SimpleFailure): DateValidator {
     TechnicalError.validateRequiredFields({ limitDate })
 
     const isValid = this._value instanceof Date && !isNaN(this._value.getTime())
 
     const date = isValid ? this._value : new Date(new Date(limitDate).setDate(limitDate.getDate() + 10000))
 
-    return this.validate(() => date > limitDate, {
-      code,
-      details: {
-        date: isValid ? date.toISOString() : 'N/A',
-        min_date: limitDate?.toISOString(),
-        ...details,
-      },
-    })
+    return this.validate(
+      () => date > limitDate,
+      failure ? failure() : FailureFactory.DATE_NOT_BEFORE_LIMIT(this._value, limitDate.toISOString())
+    )
   }
 
   /**
-   * Verifica se a data está entre as datas de início e fim (inclusivo)
-   * @param startDate Data de início
-   * @param endDate Data de fim
-   * @param code Código de erro opcional
-   * @param details Detalhes adicionais para a mensagem de erro
+   * Verifica se a data atual está entre duas datas (inclusivo).
+   * @param startDate A data de início do intervalo.
+   * @param endDate A data de fim do intervalo.
+   * @param failure (Opcional) Uma função que retorna um `SimpleFailure` customizado em caso de falha.
+   * @returns A instância atual de `DateValidator` para encadeamento de chamadas.
    */
-  public isBetween(
-    startDate: Date,
-    endDate: Date,
-    code: FailureCode = FailureCode.DATE_OUT_OF_RANGE,
-    details: Record<string, any> = {}
-  ): DateValidator {
+  public isBetween(startDate: Date, endDate: Date, failure?: () => SimpleFailure): DateValidator {
     TechnicalError.validateRequiredFields({ startDate, endDate })
 
+    if (!this._value || !(this._value instanceof Date))
+      return this.validate(() => true, FailureFactory.CONTENT_WITH_INVALID_TYPE(typeof this._value, this._field))
+
     if (startDate > endDate)
-      return this.validate(() => true, {
-        code: FailureCode.DATE_WITH_INVALID_SEQUENCE,
-        details: {
-          start_date: startDate?.toISOString(),
-          end_date: endDate?.toISOString(),
-        },
-      })
+      return this.validate(
+        () => true,
+        FailureFactory.DATE_WITH_INVALID_SEQUENCE(startDate.toISOString(), endDate.toISOString())
+      )
 
     return this.validate(
       () => {
         const time = this._value.getTime()
-        return !(time >= startDate && time <= endDate)
+        return !(time >= startDate.getTime() && time <= endDate.getTime())
       },
-      {
-        code,
-        details: {
-          date: this._value.toISOString(),
-          start_date: startDate?.toISOString(),
-          end_date: endDate?.toISOString(),
-          max_days: endDate.getDay() - startDate.getDay(),
-          ...details,
-        },
-      }
+      failure
+        ? failure()
+        : FailureFactory.DATE_IS_OUT_OF_RANGE(
+            this._value?.toISOString() || 'N/D',
+            startDate.toISOString(),
+            endDate.toISOString()
+          )
     )
   }
 }
