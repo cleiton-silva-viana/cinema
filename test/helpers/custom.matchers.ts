c001a7063d62e9890532d5135c5deae3b5ca0ee1
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-06 20:06:00 -0300

    feat(/test/helpers/): Adicionado novos matches para o `Result`.

--- test/helpers/custom.matchers.ts
+++ test/helpers/custom.matchers.ts
@@ -1,5 +1,4 @@
 import { Result } from '@shared/result/result'
-import { SimpleFailure } from '@shared/failure/simple.failure.type'
 import { FailureCode } from '@shared/failure/failure.codes.enum'
 import { TechnicalError } from '@/shared/error/technical.error'
 import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils'
@@ -14,9 +13,10 @@ declare global {
   namespace jest {
     interface Matchers<R> {
       toBeValidResult(): R
+      toBeValidResultWithValue(expectedValue: any): R
+      toBeValidResultMatching<T>(predicate: (value: T) => boolean | void): R
       toBeInvalidResult(): R
-      toHaveFailureCode(expectedCode: FailureCode): R
-      toHaveFailureCodes(expectedCodes: FailureCode[]): R
+      toBeInvalidResultWithSingleFailure(expectedCode: FailureCode): R
       toThrowTechnicalError(): R
       toHaveTechnicalErrorCode(expectedCode: FailureCode): R
     }
@@ -54,6 +54,113 @@ function toBeValidResult(received: Result<any>) {
 }
 
 /**
+ * Matcher para verificar se um Result é válido e contém um valor primitivo específico
+ * */
+function toBeValidResultWithValue(received: Result<any>, expectedValue: any) {
+  if (received.isInvalid()) {
+    const failures = received.failures.map((f) => f.code)
+    return {
+      message: () =>
+        matcherHint('.toBeValidResultWithValue', 'received', 'expectedValue') +
+        '\n\n' +
+        'Expected Result to be valid with value:\n' +
+        '  ' +
+        printExpected(expectedValue) +
+        '\n' +
+        'But Result was invalid with failures:\n' +
+        '  ' +
+        printReceived(failures),
+      pass: false,
+    }
+  }
+
+  const actualValue = received.value
+  const pass = Object.is(actualValue, expectedValue)
+
+  if (pass) {
+    return {
+      message: () =>
+        matcherHint('.not.toBeValidResultWithValue', 'received', 'expectedValue') +
+        '\n\n' +
+        'Expected Result not to have value:\n' +
+        '  ' +
+        printExpected(expectedValue) +
+        '\n' +
+        'But received:\n' +
+        '  ' +
+        printReceived(actualValue),
+      pass: true,
+    }
+  } else {
+    return {
+      message: () =>
+        matcherHint('.toBeValidResultWithValue', 'received', 'expectedValue') +
+        '\n\n' +
+        'Expected Result to have value:\n' +
+        '  ' +
+        printExpected(expectedValue) +
+        '\n' +
+        'Received:\n' +
+        '  ' +
+        printReceived(actualValue),
+      pass: false,
+    }
+  }
+}
+
+/**
+ * Matcher para verificar se um Result é válido e executa validações customizadas
+ */
+function toBeValidResultMatching<T>(received: Result<T>, predicate: (value: T) => void | boolean) {
+  if (received.isInvalid()) {
+    const failures = received.failures.map((f) => f.code).join(', ')
+    return {
+      message: () =>
+        matcherHint('.toBeValidResultMatching', 'received', 'predicate') +
+        '\n\n' +
+        'Expected Result to be valid and match predicate, but it was invalid\n' +
+        'Failures: ' +
+        printReceived(`[${failures}]`),
+      pass: false,
+    }
+  }
+
+  try {
+    const result = predicate(received.value)
+    if (result === false) {
+      return {
+        message: () =>
+          matcherHint('.not.toBeValidResultMatching', 'received', 'predicate') +
+          '\n\n' +
+          'Expected Result value not to match predicate, but it did\n' +
+          'Received value:' +
+          printReceived(received.value),
+        pass: true,
+      }
+    }
+
+    return {
+      message: () =>
+        matcherHint('.not.toBeValidResultMatching', 'received', 'predicate') +
+        '\n\n' +
+        'Expected Result value not to match predicate, but it did\n' +
+        'Received value: ' +
+        printReceived(received.value),
+      pass: true,
+    }
+  } catch (error) {
+    return {
+      message: () =>
+        matcherHint('.toBeValidResultMatching', 'received', 'predicate') +
+        '\n\n' +
+        'Expected predicate to execute without errors, but it threw:\n' +
+        printReceived(error),
+      pass: false,
+    }
+  }
+}
+
+/**
  * Matcher para verificar se um Result é inválido (failure)
  */
 function toBeInvalidResult(received: Result<any>) {
@@ -83,15 +190,15 @@ function toBeInvalidResult(received: Result<any>) {
 }
 
 /**
- * Matcher para verificar se um Result contém um código de falha específico
+ * Matcher para verificar se um Result é inválido e contém apenas uma falha com código específico
  */
-function toHaveFailureCode(received: Result<any>, expectedCode: FailureCode) {
+function toBeInvalidResultWithSingleFailure(received: Result<any>, expectedCode: FailureCode) {
   if (received.isValid()) {
     return {
       message: () =>
-        matcherHint('.toHaveFailureCode', 'received', 'expectedCode') +
+        matcherHint('.toBeInvalidResultWithSingleFailure', 'received', 'expectedCode') +
         '\n\n' +
-        'Expected Result to have failure code:\n' +
+        'Expected Result to be invalid with single failure code:\n' +
         '  ' +
         printExpected(expectedCode) +
         '\n' +
@@ -102,94 +209,56 @@ function toHaveFailureCode(received: Result<any>, expectedCode: FailureCode) {
     }
   }
 
-  const hasCode = received.failures.some((failure: SimpleFailure) => failure.code === expectedCode)
-  const actualCodes = received.failures.map((f: SimpleFailure) => f.code)
+  const failures = received.failures
+  const actualCodes = failures.map((f) => f.code)
 
-  if (hasCode) {
+  if (failures.length !== 1) {
     return {
       message: () =>
-        matcherHint('.not.toHaveFailureCode', 'received', 'expectedCode') +
+        matcherHint('.toBeInvalidResultWithSingleFailure', 'received', 'expectedCode') +
         '\n\n' +
-        'Expected Result not to have failure code:\n' +
+        'Expected Result to have exactly 1 failure, but got ' +
+        failures.length +
+        ' failures:\n' +
+        '  ' +
+        printReceived(actualCodes) +
+        '\n' +
+        'Expected single failure code:\n' +
+        '  ' +
+        printExpected(expectedCode),
+      pass: false,
+    }
+  }
+
+  const actualCode = failures[0].code
+  const pass = actualCode === expectedCode
+
+  if (pass) {
+    return {
+      message: () =>
+        matcherHint('.not.toBeInvalidResultWithSingleFailure', 'received', 'expectedCode') +
+        '\n\n' +
+        'Expected Result not to have single failure with code:\n' +
         '  ' +
         printExpected(expectedCode) +
         '\n' +
-        'But it was found in failures:\n' +
+        'But received:\n' +
         '  ' +
-        printReceived(actualCodes),
+        printReceived(actualCode),
       pass: true,
     }
   } else {
     return {
       message: () =>
-        matcherHint('.toHaveFailureCode', 'received', 'expectedCode') +
+        matcherHint('.toBeInvalidResultWithSingleFailure', 'received', 'expectedCode') +
         '\n\n' +
-        'Expected Result to have failure code:\n' +
+        'Expected Result to have single failure with code:\n' +
         '  ' +
         printExpected(expectedCode) +
         '\n' +
-        'Received failure codes:\n' +
+        'Received:\n' +
         '  ' +
-        printReceived(actualCodes),
-      pass: false,
-    }
-  }
-}
-
-/**
- * Matcher para verificar se um Result contém múltiplos códigos de falha
- */
-function toHaveFailureCodes(received: Result<any>, expectedCodes: FailureCode[]) {
-  if (received.isValid()) {
-    return {
-      message: () =>
-        matcherHint('.toHaveFailureCodes', 'received', 'expectedCodes') +
-        '\n\n' +
-        'Expected Result to have failure codes:\n' +
-        '  ' +
-        printExpected(expectedCodes) +
-        '\n' +
-        'But Result was valid with value:\n' +
-        '  ' +
-        printReceived(received.value),
-      pass: false,
-    }
-  }
-
-  const actualCodes = received.failures.map((f: SimpleFailure) => f.code)
-  const hasAllCodes = expectedCodes.every((code) => actualCodes.includes(code))
-  const missingCodes = expectedCodes.filter((code) => !actualCodes.includes(code))
-
-  if (hasAllCodes) {
-    return {
-      message: () =>
-        matcherHint('.not.toHaveFailureCodes', 'received', 'expectedCodes') +
-        '\n\n' +
-        'Expected Result not to have all failure codes:\n' +
-        '  ' +
-        printExpected(expectedCodes) +
-        '\n' +
-        'But all were found in:\n' +
-        '  ' +
-        printReceived(actualCodes),
-      pass: true,
-    }
-  } else {
-    return {
-      message: () =>
-        matcherHint('.toHaveFailureCodes', 'received', 'expectedCodes') +
-        '\n\n' +
-        'Expected Result to have failure codes:\n' +
-        '  ' +
-        printExpected(expectedCodes) +
-        '\n' +
-        'Missing codes:\n' +
-        '  ' +
-        printExpected(missingCodes) +
-        '\n' +
-        'Received failure codes:\n' +
-        '  ' +
-        printReceived(actualCodes),
+        printReceived(actualCode),
       pass: false,
     }
   }
@@ -265,9 +334,10 @@ export function setupCustomMatchers() {
   expect.extend({
     toBeValidResult,
     toBeInvalidResult,
-    toHaveFailureCode,
-    toHaveFailureCodes,
     toThrowTechnicalError,
     toHaveTechnicalErrorCode,
+    toBeValidResultMatching,
+    toBeValidResultWithValue,
+    toBeInvalidResultWithSingleFailure,
   })
 }
