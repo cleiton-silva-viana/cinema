87b193beca9a4dd347d92479b192172a4ae888e8
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-16 19:37:43 -0300

    docs(IStorageService): Adiciona a documentação a tal interface.

--- src/modules/storage/storage.service.interface.ts
+++ src/modules/storage/storage.service.interface.ts
@@ -1,15 +1,12 @@
-import { Result } from '@shared/result/result'
-import { ImageUID } from '@/modules/image/entity/value-object/image.uid'
-
-export interface FilePaths {
-  small: string
-  normal: string
-  large: string
-  uid: string
-}
+import { IFilePaths } from '@modules/storage/filepath.interface'
 
 export interface IStorageService {
-  save(basePath: string, buffers: { small: Buffer; normal: Buffer; large: Buffer }): Promise<Result<FilePaths>>
+  /**
+   * Salva os buffers das imagens no armazenamento, retornando seus caminhos de arquivo.
+   * @param basePath O caminho base onde as imagens serão armazenadas.
+   * @param imageBuffers Um objeto contendo os buffers das imagens pequena, normal e grande.
+   */
+  save(basePath: string, imageBuffers: { small: Buffer; normal: Buffer; large: Buffer }): Promise<IFilePaths>
 
-  delete(uid: ImageUID): Promise<Result<null>>
+  delete(uid: string): Promise<null>
 }
