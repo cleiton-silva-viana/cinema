f51b9158452886b97ca20c77b6d79f0e7a19496e
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-16 21:06:37 -0300

    refactor(MovieFilterDateRange): Refatora para utilizar novos matchers, nova forma de validar etc.

--- src/modules/movie/entity/value-object/movie.filter.date.range.spec.ts
+++ src/modules/movie/entity/value-object/movie.filter.date.range.spec.ts
@@ -1,96 +1,98 @@
 import { MovieFilterDateRange } from './movie.filter.date.range'
 import { FailureCode } from '@shared/failure/failure.codes.enum'
+import { DateHelper } from '@shared/helper/date.helper'
 
 describe('MovieFilterDateRange', () => {
-  let now: Date
+  describe('create', () => {
+    it('deve criar um MovieFilterDateRange válido', () => {
+      // Arrange
+      const startIn = DateHelper.soon(0)
+      const endIn = DateHelper.soon(7)
 
-  beforeEach(() => (now = new Date(new Date().setHours(0, 0, 0, 0))))
+      // Act
+      const result = MovieFilterDateRange.create(startIn, endIn)
 
-  it('deve criar um MovieFilterDateRange válido', () => {
-    // Arrange
-    const startIn = new Date(now)
-    const endIn = new Date(now)
-    endIn.setDate(endIn.getDate() + 7) // Usar endIn ao invés de now
+      // Assert
+      expect(result).toBeValidResultMatching<MovieFilterDateRange>((m) => {
+        expect(m.startDate).toEqual(startIn)
+        expect(m.endDate).toEqual(endIn)
+      })
+    })
 
-    // Act
-    const result = MovieFilterDateRange.create(startIn, endIn)
+    it('deve falhar quando data de início estiver no passado', () => {
+      // Arrange
+      const pastDate = DateHelper.recent(1)
+      const endIn = DateHelper.soon(7)
 
-    // Assert
-    expect(result).toBeValidResultMatching<MovieFilterDateRange>((m) => {
-      expect(m.startDate).toEqual(startIn)
-      expect(m.endDate).toEqual(endIn)
+      // Act
+      const result = MovieFilterDateRange.create(pastDate, endIn)
+
+      // Assert
+      expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_CANNOT_BE_PAST)
+    })
+
+    it('deve falhar quando data de inicio posterior à data de término', () => {
+      // Arrange
+      const futureDate = DateHelper.soon(MovieFilterDateRange.MAX_DATE_RANGE_DAYS + 1)
+      const endIn = DateHelper.soon(11)
+
+      // Act
+      const result = MovieFilterDateRange.create(futureDate, endIn)
+
+      // Assert
+      expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_WITH_INVALID_SEQUENCE)
+    })
+
+    it('deve falhar quando dada de fim estiver antes de data de início', () => {
+      // Arrange
+      const startIn = DateHelper.soon(0)
+      const endIn = DateHelper.recent(1)
+
+      // Act
+      const result = MovieFilterDateRange.create(startIn, endIn)
+
+      // Assert
+      expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_WITH_INVALID_SEQUENCE)
+    })
+
+    it('deve falhar quando o intervalo de datas for muito grande', () => {
+      // Arrange
+      const startIn = DateHelper.soon(0)
+      const endIn = DateHelper.soon(MovieFilterDateRange.MAX_DATE_RANGE_DAYS + 1)
+
+      // Act
+      const result = MovieFilterDateRange.create(startIn, endIn)
+
+      // Assert
+      expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_RANGE_TOO_LARGE)
+    })
+
+    it('deve retornar result de falha com múltiplas falhas', () => {
+      // Arrange
+      const pastDate = DateHelper.recent(1)
+      const futureDate = DateHelper.soon(1)
+
+      // Act
+      const result = MovieFilterDateRange.create(pastDate, futureDate)
+
+      // Assert
+      expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_CANNOT_BE_PAST)
     })
   })
 
-  it('deve retornar um MovieFilterDateRange padrão', () => {
-    // Act
-    const result = MovieFilterDateRange.createDefault()
+  describe('createDefault', () => {
+    it('deve retornar um MovieFilterDateRange padrão', () => {
+      // Arrange
+      const expectedStartDate = DateHelper.startOfDay(new Date())
+      const expectedEndDate = DateHelper.endOfDay(DateHelper.soon(MovieFilterDateRange.DEFAULT_DATE_RANGE_DAYS))
 
-    // Assert
-    expect(result).toBeDefined()
-    const expectedEndDate = new Date(now)
-    expectedEndDate.setDate(expectedEndDate.getDate() + 7)
-    expect(result.startDate).toEqual(now)
-    expect(result.endDate).toEqual(expectedEndDate)
-  })
+      // Act
+      const result = MovieFilterDateRange.createDefault()
 
-  it('deve falhar quando data de início estiver no passado', () => {
-    // Arrange
-    const pastDate = new Date(now.getTime() - 1 * 60000)
-    const endIn = new Date(now.getTime() + 7 * 60000) // 7 dias no futuro
-
-    // Act
-    const result = MovieFilterDateRange.create(pastDate, endIn)
-
-    // Assert
-    expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_CANNOT_BE_PAST)
-  })
-
-  it('deve falhar quando data de for posterior à data de término', () => {
-    // Arrange
-    const futureDate = new Date(now.getTime() + (MovieFilterDateRange.MAX_DATE_RANGE_DAYS + 1) * 60000)
-    const endIn = new Date(now.getTime() + 1 * 60000) // 1 dia no futuro
-
-    // Act
-    const result = MovieFilterDateRange.create(futureDate, endIn)
-
-    // Assert
-    expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_WITH_INVALID_SEQUENCE)
-  })
-
-  it('deve falhar quando dada de fim estiver antes de data de início', () => {
-    // Arrange
-    const startIn = new Date(now)
-    const endIn = new Date(now.getTime() - 1 * 60000) // 1 dia no passado
-
-    // Act
-    const result = MovieFilterDateRange.create(startIn, endIn)
-
-    // Assert
-    expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_WITH_INVALID_SEQUENCE)
-  })
-
-  it('deve falhar quando o intervalo de datas for muito grande', () => {
-    // Arrange
-    const startIn = new Date(now)
-    const endIn = new Date(now.getTime() + (MovieFilterDateRange.MAX_DATE_RANGE_DAYS + 1) * 24 * 60 * 60 * 1000)
-
-    // Act
-    const result = MovieFilterDateRange.create(startIn, endIn)
-
-    // Assert
-    expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_NOT_BEFORE_LIMIT)
-  })
-
-  it('deve retornar result de falha com múltiplas falhas', () => {
-    // Arrange
-    const pastDate = new Date(now.getTime() - 1 * 60000) // 1 dia no passado
-    const futureDate = new Date(now.getTime() + (MovieFilterDateRange.MAX_FUTURE_DAYS + 1) * 60000)
-
-    // Act
-    const result = MovieFilterDateRange.create(pastDate, futureDate)
-
-    // Assert
-    expect(result).toBeInvalidResultWithSingleFailure(FailureCode.DATE_CANNOT_BE_PAST)
+      // Assert
+      expect(result).toBeDefined()
+      expect(result.startDate).toEqual(expectedStartDate)
+      expect(result.endDate).toEqual(expectedEndDate)
+    })
   })
 })
--- src/modules/movie/entity/value-object/movie.filter.date.range.ts
+++ src/modules/movie/entity/value-object/movie.filter.date.range.ts
@@ -2,7 +2,7 @@ import { failure, Result, success } from '@shared/result/result'
 import { SimpleFailure } from '@shared/failure/simple.failure.type'
 import { Validate } from '@shared/validator/validate'
 import { FailureFactory } from '@shared/failure/failure.factory'
-import { DateUtils } from '@shared/utils/date.utils'
+import { DateHelper } from '@shared/helper/date.helper'
 
 export class MovieFilterDateRange {
   private constructor(
@@ -21,51 +21,68 @@ export class MovieFilterDateRange {
   public static readonly MAX_DATE_RANGE_DAYS = 14
 
   /**
+   * Intervalo padrão utilizado pelo método createDefault
+   * */
+  public static readonly DEFAULT_DATE_RANGE_DAYS = 7
+
+  /**
    * Cria um novo intervalo de datas com validação
    */
   public static create(startDate: Date, endDate: Date): Result<MovieFilterDateRange> {
+    const today = DateHelper.startOfDay(new Date())
     const failures: SimpleFailure[] = []
-    const today = new Date()
-    today.setHours(0, 0, 0, 0)
 
-    const maxFutureDate = new Date(today)
-    maxFutureDate.setDate(today.getDate() + this.MAX_FUTURE_DAYS)
+    const maxFutureDate = DateHelper.endOfDay(DateHelper.addDays(this.MAX_FUTURE_DAYS, today))
+
+    /*
+      Abordagem que deve ser utilizada!
+
+       return (Validate
+        .date({startDate}, failures)
+        .isRequired()
+        .isAfter(today, () => FailureFactory.DATE_CANNOT_BE_PAST('startDate'))
+        .isBefore(maxFutureDate))
+          .map(() => {
+            const maxEndDate = DateHelper.addDays(this.MAX_FUTURE_DAYS, today)
+            Validate.date({endDate}, failures)
+                .isRequired()
+                .isAfter(startDate, () => FailureFactory.DATE_WITH_INVALID_SEQUENCE(DateHelper.formatDateToISOString(startDate), DateHelper.formatDateToISOString(endDate)))
+                .isBefore(maxEndDate)
+          })
+          .fold((result) => {
+            onFailure: result
+            onSuccess: new MovieFilterDateRange(startDate, endDate)
+          })*/
 
     Validate.date({ startDate }, failures)
       .isRequired()
       .isAfter(today, () => FailureFactory.DATE_CANNOT_BE_PAST('startDate'))
       .isBefore(maxFutureDate)
-
-    if (failures.length === 0) {
-      const maxEndDate = new Date()
-      maxEndDate.setDate(maxFutureDate.getDate() + this.MAX_DATE_RANGE_DAYS)
-
-      Validate.date({ endDate }, failures)
-        .isRequired()
-        .isAfter(startDate, () =>
-          FailureFactory.DATE_WITH_INVALID_SEQUENCE(
-            DateUtils.formatDateToISOString(startDate),
-            DateUtils.formatDateToISOString(endDate)
+      .then(() => {
+        const maxEndDate = DateHelper.addDays(this.MAX_FUTURE_DAYS, today)
+        Validate.date({ endDate }, failures)
+          .isRequired()
+          .isAfter(startDate, () =>
+            FailureFactory.DATE_WITH_INVALID_SEQUENCE(
+              DateHelper.formatDateToISOString(startDate),
+              DateHelper.formatDateToISOString(endDate)
+            )
           )
-        )
-        .isBefore(maxEndDate)
-    }
+          .isBefore(maxEndDate)
+          .isBefore(DateHelper.addDays(MovieFilterDateRange.MAX_DATE_RANGE_DAYS, startDate), () =>
+            FailureFactory.DATE_RANGE_TOO_LARGE(MovieFilterDateRange.MAX_DATE_RANGE_DAYS)
+          )
+      })
 
-    if (failures.length > 0) return failure(failures)
-
-    return success(new MovieFilterDateRange(startDate, endDate))
+    return failures.length > 0 ? failure(failures) : success(new MovieFilterDateRange(startDate, endDate))
   }
 
   /**
    * Cria um intervalo de datas padrão (hoje até hoje + 7 dias)
    */
   public static createDefault(): MovieFilterDateRange {
-    const today = new Date()
-    today.setHours(0, 0, 0, 0)
-
-    const endDate = new Date(today)
-    endDate.setDate(endDate.getDate() + 7)
-
+    const today = DateHelper.startOfDay(new Date())
+    const endDate = DateHelper.endOfDay(DateHelper.soon(MovieFilterDateRange.DEFAULT_DATE_RANGE_DAYS))
     return new MovieFilterDateRange(today, endDate)
   }
 }
