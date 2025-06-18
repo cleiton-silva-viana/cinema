baed9fa43906041264a83a29d7c6b5ecbcae9f52
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-18 13:05:50 -0300

    feat(ScreeningAdministrativeStatusEnum): Cria um enum que representa os possíveis status administrativos de uma exibição.

---
+++ src/modules/screening/enum/screening.administrative.status.ts
@@ -0,0 +1,8 @@
+/** Define os possíveis estados de uma sessão de exibição. */
+export enum ScreeningAdministrativeStatusEnum {
+    /** Agendada normalmente. */
+    SCHEDULED = 'SCHEDULED',
+
+    /** Cancelada (não ocorrerá). */
+    CANCELLED = 'CANCELLED',
+}
