15f63210d45c7cbfd86454bcf99e0cda38d4d656
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-17 21:51:10 -0300

    refactor(test/builder.movie.builder): Atualiza este builder para ser mais aderente as necessidades dos testes atuais.

--- test/builder/movie.builder.ts
+++ test/builder/movie.builder.ts
@@ -1,8 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { IMovieHydrateInput, Movie } from '@modules/movie/entity/movie'
-import { ICreateMovieDTO } from '@modules/movie/controller/dto/movie.dto'
 import { ImageUID } from '@modules/image/entity/value-object/image.uid'
-import { PersonRole } from '@/modules/movie/entity/value-object/movie.contributor'
+import { IMovieContributorInput, PersonRole } from '@/modules/movie/entity/value-object/movie.contributor'
 import { Genre } from '@modules/movie/entity/value-object/movie.genre'
 import { MovieUID } from '@modules/movie/entity/value-object/movie.uid'
 import { MovieAdministrativeStatusEnum } from '@modules/movie/type/movie.administrative.status'
@@ -10,6 +9,12 @@ import { AgeRatingEnum } from '@modules/movie/entity/value-object/age.rating'
 import { SupportedLanguageEnum } from '@/shared/value-object/language-content/supported.language.enum'
 import { CreateTestContributorInput } from '@test/builder/contributor.builder'
 import { DateHelper } from '@shared/helper/date.helper'
+import { IMovieWithRelations } from '@modules/movie/repository/movie.repository.interface'
+import { IHydrateImageParams } from '@modules/image/entity/image'
+import { CreateTestImage } from '@test/builder/image.builder'
+import { hydrateEnum } from '@shared/validator/utils/validation.helpers'
+import { Person } from '@modules/person/entity/person'
+import { CreateTestPerson } from '@test/builder/person.builder'
 
 export function CreateTestMovie(override?: Partial<IMovieHydrateInput>) {
   let { uid, title, description, ageRating, imageUID, duration, contributors, genres, status, displayPeriod } =
@@ -54,23 +59,33 @@ export function CloneTestMovieWithOverrides(movie: Movie, override: Partial<Omit
   })
 }
 
-export function CreateTestMovieDTO(override?: Partial<ICreateMovieDTO>): ICreateMovieDTO {
+export function CreateTestMovieWithRelations(options?: {
+  movie?: Partial<IMovieHydrateInput>
+  image?: Partial<IHydrateImageParams>
+  contributos?: Partial<IMovieContributorInput>[]
+}): IMovieWithRelations {
+  const image = CreateTestImage(options?.image)
+
+  const contributorsInput: IMovieContributorInput[] = []
+  const contributors: Person[] = []
+
+  if (options?.contributos) {
+    options.contributos.forEach((ctb) => {
+      const role = ctb.role ? hydrateEnum({ role: ctb.role }, PersonRole) : undefined
+      contributorsInput.push(CreateTestContributorInput(role, ctb.personUID))
+      contributors.push(CreateTestPerson({ uid: ctb.personUID }))
+    })
+  }
+
+  const movie = CreateTestMovie({
+    ...options?.movie,
+    contributors: contributorsInput,
+  })
+
   return {
-    title: override?.title ?? [
-      { text: faker.lorem.sentences({ min: 3, max: 5 }), language: SupportedLanguageEnum.PT },
-      { text: faker.lorem.sentences({ min: 3, max: 5 }), language: SupportedLanguageEnum.EN },
-    ],
-    description: override?.description ?? [
-      { text: faker.lorem.paragraph(), language: SupportedLanguageEnum.PT },
-      { text: faker.lorem.paragraph(), language: SupportedLanguageEnum.EN },
-    ],
-    ageRating: override?.ageRating ?? faker.helpers.enumValue(Genre),
-    imageUID: override?.imageUID ?? ImageUID.create().value,
-    contributors: override?.contributors ?? [
-      { personUID: faker.string.uuid(), role: PersonRole.DIRECTOR },
-      { personUID: faker.string.uuid(), role: PersonRole.ACTOR },
-    ],
-    duration: override?.duration ?? faker.number.int({ min: 90, max: 180 }),
+    movie,
+    contributors,
+    image,
   }
 }
 
