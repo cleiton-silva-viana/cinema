18651dfc94ec0b53c690c5c5c4a3e54ef823d6ef
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-19 18:17:48 -0300

    refactor(ICustomerApplicationService): Altera os imports.

--- src/modules/customer/service/customer.application.service.interface.ts
+++ src/modules/customer/service/customer.application.service.interface.ts
@@ -2,28 +2,17 @@ import { CustomerUID } from '@modules/customer/entity/value-object/customer.uid'
 import { Result } from '@shared/result/result'
 import { Customer } from '@modules/customer/entity/customer'
 import { Email } from '@modules/customer/entity/value-object/email'
-
-export interface ICreateCustomerProps {
-  name: string
-  birthDate: Date
-  email: string
-  password: string
-}
-
-export interface IStudentCardInput {
-  id: string
-  validity: Date
-}
+import { ICreateCustomerCommand, IStudentCardCommand } from '@modules/customer/interface/customer.command.interface'
 
 export interface ICustomerApplicationService {
   findById(uid: string | CustomerUID): Promise<Result<Customer>>
   findByEmail(email: string | Email): Promise<Result<Customer>>
-  create(props: ICreateCustomerProps): Promise<Result<Customer>>
+  create(props: ICreateCustomerCommand): Promise<Result<Customer>>
   updateCustomerEmail(customerUID: string, email: string): Promise<Result<Customer>>
   updateCustomerName(customerUID: string, newName: string): Promise<Result<Customer>>
   updateCustomerBirthDate(customerUID: string, birthDate: Date): Promise<Result<Customer>>
   assignCustomerCPF(customerUID: string, cpf: string): Promise<Result<Customer>>
   removeCustomerCPF(customerUID: string): Promise<Result<Customer>>
   removeCustomerStudentCard(customerUID: string): Promise<Result<Customer>>
-  assignCustomerStudentCard(customerUID: string, studentCard: IStudentCardInput): Promise<Result<Customer>>
+  assignCustomerStudentCard(customerUID: string, studentCard: IStudentCardCommand): Promise<Result<Customer>>
 }
