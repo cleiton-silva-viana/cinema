2176ec626577702189752198b13c602cfc64bfe9
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-20 20:37:15 -0300

    refactor(StudentCardModel): Altera para ser uma tabela mais simples, apenas para conter dados fundamentais de uma carteira de estudante.

--- src/modules/customer/repository/model/student.card.model.ts
+++ src/modules/customer/repository/model/student.card.model.ts
@@ -1,36 +1,28 @@
-import {Column, Entity, JoinColumn, OneToOne, PrimaryColumn} from 'typeorm'
-import {CustomerModel} from './customer.model'
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm'
+import { CustomerModel } from './customer.model'
 
 @Entity('student_card')
 export class StudentCardModel {
-    @PrimaryColumn({type: 'varchar', nullable: false, unique: true})
-    public readonly uid: string
+  @PrimaryColumn({ type: 'varchar', nullable: false, unique: true })
+  public readonly customerUID: string
 
-    @Column({type: 'varchar', nullable: false})
-    public readonly registrationNumber: string
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  public readonly registrationNumber: string
 
-    @Column({type: 'varchar', nullable: false})
-    public readonly institution: string
+  @Column({ type: 'varchar', nullable: false })
+  public readonly institution: string
 
-    @Column({type: 'datetime', nullable: false})
-    public readonly expiresAt: Date
+  @Column({ type: 'datetime', nullable: false })
+  public readonly expiresAt: Date
 
-    @Column({type: 'datetime'})
-    public readonly createdAt: Date
+  @OneToOne(() => CustomerModel, (customer) => customer.studentCard)
+  @JoinColumn({ name: 'customerUID' })
+  public customer?: CustomerModel
 
-    @Column({type: 'datetime'})
-    public readonly updatedAt: Date
-
-    @OneToOne(() => CustomerModel, (customer) => customer.studentCard)
-    @JoinColumn({name: 'customerUID'})
-    public customer?: CustomerModel
-
-    constructor(uid: string, number: string, institution: string, expiresAt: Date, createdAt: Date, updatedAt: Date) {
-        this.uid = uid
-        this.registrationNumber = number
-        this.institution = institution
-        this.expiresAt = expiresAt
-        this.createdAt = createdAt
-        this.updatedAt = updatedAt
-    }
+  constructor(customerUID: string, number: string, institution: string, expiresAt: Date) {
+    this.customerUID = customerUID
+    this.registrationNumber = number
+    this.institution = institution
+    this.expiresAt = expiresAt
+  }
 }
