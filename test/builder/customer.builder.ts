4f4cb5110af824b9e391789ebcfac566e7773912
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-23 16:35:56 -0300

    feat(test/builder/customer.builder): Adiciona o atributo `status` ao builder.

--- test/builder/customer.builder.ts
+++ test/builder/customer.builder.ts
@@ -3,6 +3,7 @@ import { Customer } from '@modules/customer/entity/customer'
 import { CustomerUID } from '@modules/customer/entity/value-object/customer.uid'
 import { IHydrateCustomerCommand, IStudentCardCommand } from '@modules/customer/interface/customer.command.interface'
 import { DateHelper } from '@shared/helper/date.helper'
+import { CustomerStatusEnum } from '@modules/customer/enum/customer.status.enum'
 
 /**
  * Cria uma instância válida da classe Customer
@@ -17,8 +18,9 @@ export function CreateTestCustomer(override?: Partial<IHydrateCustomerCommand>) 
   const updatedAt = override?.updatedAt ?? DateHelper.recent(60)
   const cpf = override?.cpf
   const studentCard = override?.studentCard
+  const status = override?.status ?? CustomerStatusEnum.ACTIVE
 
-  return Customer.hydrate({ uid, name, birthDate, email, createdAt, updatedAt, cpf, studentCard })
+  return Customer.hydrate({ uid, name, birthDate, email, status, createdAt, updatedAt, cpf, studentCard })
 }
 
 export function CloneTestCustomerWithOverrides(
@@ -38,6 +40,7 @@ export function CloneTestCustomerWithOverrides(
     name: override?.name ?? customer.name.value,
     birthDate: override?.birthDate ?? customer.birthDate.value,
     email: override?.email ?? customer.email.value,
+    status: override.status ?? customer.status,
     createdAt: override.createdAt ?? customer.createdAt,
     updatedAt: customer.updatedAt ?? customer.updatedAt,
     cpf: override.hasOwnProperty('cpf') ? override.cpf : customer.cpf?.value,
