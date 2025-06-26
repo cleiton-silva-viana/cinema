779892ab7c009e1b8640d6917a3d086d155b020c
Cleiton Viana <estudo.cleiton@gmail.com>
2025-06-25 21:53:28 -0300

    refactor(ICustomerApplicationService): Move tal interface para o diretório /customer/interface.

--- src/modules/customer/interface/customer.application.service.interface.ts
+++ src/modules/customer/interface/customer.application.service.interface.ts
@@ -3,6 +3,7 @@ import { Result } from '@shared/result/result'
 import { Customer } from '@modules/customer/entity/customer'
 import { Email } from '@modules/customer/entity/value-object/email'
 import { ICreateCustomerCommand, IStudentCardCommand } from '@modules/customer/interface/customer.command.interface'
+import { CustomerStatusEnum } from '@modules/customer/enum/customer.status.enum'
 
 export interface ICustomerApplicationService {
   findById(uid: string | CustomerUID): Promise<Result<Customer>>
@@ -15,4 +16,6 @@ export interface ICustomerApplicationService {
   removeCustomerCPF(customerUID: string): Promise<Result<Customer>>
   removeCustomerStudentCard(customerUID: string): Promise<Result<Customer>>
   assignCustomerStudentCard(customerUID: string, studentCard: IStudentCardCommand): Promise<Result<Customer>>
+  requestAccountDeletion(customerUID: string): Promise<Result<Customer>>
+  reactivateAccount(customerUID: string): Promise<Result<Customer>>
 }
